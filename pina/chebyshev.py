1bc9d5c8c6b98a7269397a2c5ff9f2a1fed6733d
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-02-16 23:48:59 +0100

    use 'np.polynomial.chebyshev.chebroots' instead of 'np.roots'

--- pina/chebyshev.py
+++ pina/chebyshev.py
@@ -1,6 +1,7 @@
-from mpmath import chebyt, chop, taylor
 import numpy as np
 def chebyshev_roots(n):
     """ Return the roots of *n* Chebyshev polynomials (between [-1, 1]) """
-    return np.roots(chop(taylor(lambda x: chebyt(n, x), 0, n))[::-1])
+    coefficents = np.zeros(n+1)
+    coefficents[-1] = 1
+    return np.polynomial.chebyshev.chebroots(coefficents)
 
