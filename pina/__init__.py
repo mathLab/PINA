52b0b45045c8ef8c315abcc8356a273e17bb3540
Nicola Demo <demo.nicola@gmail.com>
2022-11-29 16:38:14 +0100

    Update __init__.py

--- pina/__init__.py
+++ pina/__init__.py
@@ -1,16 +1,16 @@
 __all__ = [
     'PINN',
-    'ParametricPINN',
     'LabelTensor',
     'Plotter',
     'Condition',
-    'Span'
+    'Span',
+    'Location',
 ]
 
 from .meta import *
 from .label_tensor import LabelTensor
 from .pinn import PINN
-#from .ppinn import ParametricPINN
 from .plotter import Plotter
 from .span import Span
 from .condition import Condition
+from .location import Location
