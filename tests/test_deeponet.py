6af60a593c678d22b3f8316947ecf60aa61235b0
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-01-04 09:23:16 +0100

    adapt tests to new interface

--- tests/test_deeponet.py
+++ tests/test_deeponet.py
@@ -1,9 +1,9 @@
-import torch
 import pytest
+import torch
 
 from pina import LabelTensor
-from pina.model import DeepONet, FeedForward as FFN
-
+from pina.model import DeepONet
+from pina.model import FeedForward as FFN
 
 data = torch.rand((20, 3))
 input_vars = ['a', 'b', 'c']
@@ -14,19 +14,17 @@ input_ = LabelTensor(data, input_vars)
 def test_constructor():
     branch = FFN(input_variables=['a', 'c'], output_variables=20)
     trunk = FFN(input_variables=['b'], output_variables=20)
-    onet = DeepONet(trunk_net=trunk, branch_net=branch,
-                    output_variables=output_vars)
+    onet = DeepONet(nets=[trunk, branch], output_variables=output_vars)
 
 def test_constructor_fails_when_invalid_inner_layer_size():
     branch = FFN(input_variables=['a', 'c'], output_variables=20)
     trunk = FFN(input_variables=['b'], output_variables=19)
     with pytest.raises(ValueError):
-        DeepONet(trunk_net=trunk, branch_net=branch, output_variables=output_vars)
+        DeepONet(nets=[trunk, branch], output_variables=output_vars)
 
 def test_forward():
     branch = FFN(input_variables=['a', 'c'], output_variables=10)
     trunk = FFN(input_variables=['b'], output_variables=10)
-    onet = DeepONet(trunk_net=trunk, branch_net=branch,
-                    output_variables=output_vars)
+    onet = DeepONet(nets=[trunk, branch], output_variables=output_vars)
     output_ = onet(input_)
     assert output_.labels == output_vars
