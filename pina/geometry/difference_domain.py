82f28b694947eaea6d4b00130be3368f7125a4e6
SpartaKushK <kkhamesra3@gmail.com>
2023-06-19 12:39:43 +0200

    added test cases and reverted difference

--- pina/geometry/difference_domain.py
+++ pina/geometry/difference_domain.py
@@ -1,7 +1,6 @@
 """Module for Location class."""
-import torch
+
 from .location import Location
-from ..utils import check_consistency
 from ..label_tensor import LabelTensor
 
 
@@ -9,76 +8,15 @@ class Difference(Location):
     """
     """
 
-    def __init__(self, geometries):
+    def __init__(self, first, second):
 
-        super().__init__()
-
-        # difference checks
-        check_consistency(geometries, Location)
-        self._check_difference_dimesions(geometries)
-
-        # assign geometries
-        self._geometries = geometries
-
-    @property
-    def geometries(self):
-        """ 
-        The geometries."""
-        return self._geometries
-
-    @property
-    def variables(self):
-        """
-        Spatial variables.
-
-        :return: All the spatial variables defined in '__init__()' in order.
-        :rtype: list[str]
-        """
-        all_variables = []
-        seen_variables = set()
-        for geometry in self.geometries:
-            for variable in geometry.variables:
-                if variable not in seen_variables:
-                    all_variables.append(variable)
-                    seen_variables.add(variable)
-        return all_variables
-
-    def is_inside(self, point, check_border=False):
-        """Check if a point is inside the difference domain.
-
-        :param point: Point to be checked.
-        :type point: torch.Tensor   
-        :param bool check_border: If True, the border is considered inside.
-        :return: True if the point is inside the union domain, False otherwise.
-        :rtype: bool
-        """
-        flag = 0
-        for geometry in self.geometries:
-            if geometry.is_inside(point, check_border):
-                flag += 1
-        return flag == 1
+        self.first = first
+        self.second = second
 
     def sample(self, n, mode='random', variables='all'):
-        sampled_points = []
-        remainder = n % len(self.geometries)
-        num_points = n // len(self.geometries)
-
-        for i, geometry in enumerate(self.geometries):
-            if i < remainder:
-                num_points += 1
-            points = geometry.sample(num_points, mode, variables)
-
-            for point in points:
-                point.labels = [f'{i}' for i in self.variables]
-                if self.is_inside(point):
-                    sampled_points.append(point)
-
-        return LabelTensor(torch.cat(sampled_points), labels=[f'{i}' for i in self.variables])
-
-    def og_sample(self, n, mode='random', variables='all'):
         """
         """
-        # assert mode is 'random', 'Only random mode is implemented'
+        assert mode is 'random', 'Only random mode is implemented'
 
         samples = []
         while len(samples) < n:
@@ -88,14 +26,3 @@ class Difference(Location):
 
         import torch
         return LabelTensor(torch.tensor(samples), labels=['x', 'y'])
-
-    def _check_difference_dimesions(self, geometries):
-        """Check if the dimensions of the geometries are consistent.
-
-        :param geometries: Geometries to be checked.
-        :type geometries: list[Location]
-        """
-        for geometry in geometries:
-            if geometry.variables != geometries[0].variables:
-                raise NotImplementedError(
-                    f'The geometries need to be the same dimensions. {geometry.variables} is not equal to {geometries[0].variables}')
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,17 @@ import torch
 
 from pina.utils import merge_tensors
 from pina.label_tensor import LabelTensor
+from pina import LabelTensor
+from pina.geometry import Union, EllipsoidDomain, CartesianDomain
+from pina.utils import check_consistency
+import pytest
+from pina.geometry import Location
+
+cart1 = CartesianDomain({'x': [0, 1], 'y': [0, 1]})
+ellipsoid1 = EllipsoidDomain({'x': [1, 2], 'y': [-2, 1]})
+union = Union([cart1, ellipsoid1])
+example_input_pts = LabelTensor(torch.tensor([[0, 0, 0]]), ['x', 'y', 'z'])
+
 
 def test_merge_tensors():
     tensor1 = LabelTensor(torch.rand((20, 3)), ['a', 'b', 'c'])
@@ -9,7 +20,23 @@ def test_merge_tensors():
     tensor3 = LabelTensor(torch.ones((30, 3)), ['g', 'h', 'i'])
 
     merged_tensor = merge_tensors((tensor1, tensor2, tensor3))
-    assert tuple(merged_tensor.labels) == ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')
+    assert tuple(merged_tensor.labels) == (
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')
     assert merged_tensor.shape == (20*20*30, 9)
     assert torch.all(merged_tensor.extract(('d', 'e', 'f')) == 0)
     assert torch.all(merged_tensor.extract(('g', 'h', 'i')) == 1)
+
+
+def test_check_consistency_correct():
+    check_consistency(example_input_pts, torch.Tensor)
+    check_consistency(union, Location)
+    check_consistency(ellipsoid1, Location)
+
+
+def test_check_consistency_incorrect():
+    with pytest.raises(ValueError):
+        check_consistency(example_input_pts, Location)
+    with pytest.raises(ValueError):
+        check_consistency(union, torch.Tensor)
+    with pytest.raises(ValueError):
+        check_consistency(ellipsoid1, torch.Tensor)
