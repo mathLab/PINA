1fbf4ad3a60e1a900b2d5f875af537eb7527edb5
benv123 <benvolokh@gmail.com>
2023-06-20 16:15:03 +0200

    Triangle geometry * creating TriangleDomain * tests

--- pina/geometry/__init__.py
+++ pina/geometry/__init__.py
@@ -21,6 +21,10 @@ from .ellipsoid import EllipsoidDomain
 from .exclusion_domain import Exclusion
 from .intersection_domain import Intersection
 from .union_domain import Union
+<<<<<<< HEAD
 from .difference_domain import Difference
 from .operation_interface import OperationInterface
 from .simplex import SimplexDomain
+=======
+from .triangle import TriangleDomain
+>>>>>>> 053d570 (Triangle geometry)
