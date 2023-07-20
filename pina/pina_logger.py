7af80e89004d92bb5c05127b86f3c32e79066e36
SpartaKushK <kkhamesra3@gmail.com>
2023-07-20 15:24:12 +0200

    Abstraction of Loggers

---
+++ pina/pina_logger.py
@@ -0,0 +1,31 @@
+
+from abc import abstractmethod, ABCMeta
+from lightning.pytorch.loggers.logger import Logger, rank_zero_experiment
+from lightning.pytorch.utilities import rank_zero_only
+
+
+class PINALogger(Logger, metaclass=ABCMeta):
+    """ Base class for all loggers in Lightning """
+
+    @abstractmethod
+    def call(self, model):
+        """Lightning calls this hook with the trainer"""
+        pass
+
+
+# import lightning.pytorch as pl
+
+# class PinaLogger(object):
+#     """
+#     PINA logger wrapper
+#     """
+
+#     def __init__(self, loggers) -> None:
+#         self.loggers = loggers
+
+#         if len(self.loggers) > 1:
+#             raise NotImplementedError(
+#                 'Currently implementing one logger at a time.')
+
+#     def log_graph(self, model):
+#         self.loggers[0].log_graph(model=model)
---
+++ pina/pina_tensorboard.py
@@ -0,0 +1,9 @@
+
+from pina.pina_logger import PINALogger
+
+
+class PinaTensorBoard(PINALogger):
+    """ Base class for all loggers in TensorBoard """
+
+    def call():
+        pass
