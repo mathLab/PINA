15ecaacb7c029f65a59a1de853ac4e083b861317
Dario Coscia <dariocoscia@cli-10-110-2-174.WIFIeduroamSTUD.units.it>
2023-08-01 19:13:36 +0200

    Layers and Models update PR * add residual block * add test conv and residual block * modify FFN kwargs

---
+++ pina/model/layers/spectral.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from ...utils import check_consistency
+
+
+class SpectralConvBlock(nn.Module):
+    """
+    Implementation of spectral convolution block.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        pass
