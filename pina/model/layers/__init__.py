b7f9694cb985f0cdf916ee77c26e9710c2147a3f
Dario Coscia <dariocoscia@cli-10-110-1-156.WIFIeduroamSTUD.units.it>
2023-09-07 17:13:51 +0200

    Spectral Convolution Addition * Implementing 1D/2D/3D spectral conv * Implementing tests for 1D/2D/3d spectral conv

--- pina/model/layers/__init__.py
+++ pina/model/layers/__init__.py
@@ -1,7 +1,11 @@
 __all__ = [
     'ContinuousConvBlock',
-    'ResidualBlock'
+    'ResidualBlock',
+    'SpectralConvBlock1D',
+    'SpectralConvBlock2D',
+    'SpectralConvBlock3D'
 ]
 
 from .convolution_2d import ContinuousConvBlock
 from .residual import ResidualBlock
+from .spectral import SpectralConvBlock1D, SpectralConvBlock2D, SpectralConvBlock3D
