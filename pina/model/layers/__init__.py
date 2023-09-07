83ecdb0eab8acd8b74bb1d3f17890a7347503997
Dario Coscia <dariocoscia@cli-10-110-1-156.WIFIeduroamSTUD.units.it>
2023-09-07 18:18:28 +0200

    Fourier Block and minor fixes * Adding fourier block 1d/2d/3d * Adding docs to SpectralConvBlock1D/2D/3D and to FourierBlock1D/2D/3D * Adding tests for fourier block

--- pina/model/layers/__init__.py
+++ pina/model/layers/__init__.py
@@ -3,9 +3,13 @@ __all__ = [
     'ResidualBlock',
     'SpectralConvBlock1D',
     'SpectralConvBlock2D',
-    'SpectralConvBlock3D'
+    'SpectralConvBlock3D',
+    'FourierBlock1D',
+    'FourierBlock2D',
+    'FourierBlock3D',
 ]
 
 from .convolution_2d import ContinuousConvBlock
 from .residual import ResidualBlock
 from .spectral import SpectralConvBlock1D, SpectralConvBlock2D, SpectralConvBlock3D
+from .fourier import FourierBlock1D, FourierBlock2D, FourierBlock3D
