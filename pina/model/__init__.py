d90675b5053fd76c161a1a3f310a584c48901cef
Dario Coscia <dariocoscia@cli-10-110-1-156.WIFIeduroamSTUD.units.it>
2023-09-13 09:46:52 +0200

    MIONet addition and DeepONet modification. * Create MIONet base class * DeepONet inherits from MIONet

--- pina/model/__init__.py
+++ pina/model/__init__.py
@@ -2,10 +2,11 @@ __all__ = [
     'FeedForward',
     'MultiFeedForward',
     'DeepONet',
+    'MIONet',
     'FNO',
 ]
 
 from .feed_forward import FeedForward
 from .multi_feed_forward import MultiFeedForward
-from .deeponet import DeepONet
+from .deeponet import DeepONet, MIONet
 from .fno import FNO
