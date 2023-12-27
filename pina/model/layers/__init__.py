eed9d7ded3927df9f6723c28c484ddf3bcb218b7
Nicola Demo <demo.nicola@gmail.com>
2023-12-27 16:01:37 +0100

    Doc and tutorial for PODLayer

--- pina/model/layers/__init__.py
+++ pina/model/layers/__init__.py
@@ -8,9 +8,11 @@ __all__ = [
     'FourierBlock1D',
     'FourierBlock2D',
     'FourierBlock3D',
+    'PODLayer'
 ]
 
 from .convolution_2d import ContinuousConvBlock
 from .residual import ResidualBlock, EnhancedLinear
 from .spectral import SpectralConvBlock1D, SpectralConvBlock2D, SpectralConvBlock3D
 from .fourier import FourierBlock1D, FourierBlock2D, FourierBlock3D
+from .pod import PODLayer
