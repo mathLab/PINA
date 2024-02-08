6fe17bf5951079a6ca2427781274c9ebc3410701
cyberguli <guglielmo.padula@aruba.it>
2024-02-08 16:10:22 +0100

    added GNO with test and tutorial

--- pina/model/__init__.py
+++ pina/model/__init__.py
@@ -5,9 +5,11 @@ __all__ = [
     'DeepONet',
     'MIONet',
     'FNO',
+    'GNO'
 ]
 
 from .feed_forward import FeedForward, ResidualFeedForward
 from .multi_feed_forward import MultiFeedForward
 from .deeponet import DeepONet, MIONet
 from .fno import FNO
+from .gno import GNO
