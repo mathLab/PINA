2e9d2adc3157720680deb6114cefb44a585b9283
cyberguli <guglielmo.padula@aruba.it>
2024-02-17 17:32:02 +0100

    reverting dataclass as only worsens things

--- pina/model/layers/avno_layer.py
+++ pina/model/layers/avno_layer.py
@@ -1,9 +1,6 @@
 """Module for Averaging Neural Operator Layer class."""
 from torch import nn, mean
-import dataclasses
-from collections.abc import Callable
 
-@dataclasses.dataclass
 class AVNOLayer(nn.Module):
     """
     The PINA implementation of the inner layer 
@@ -12,15 +9,16 @@ class AVNOLayer(nn.Module):
     :param int hidden_size: size of the layer.
     :param func: the activation function to use. 
     """
-
-    hidden_size: int
-    func: Callable
-
-    def __post_init__(self):
+    def __init__(self, hidden_size: int, func=nn.GELU):
         super().__init__()
+        self.hidden_size = hidden_size
         self.nn = nn.Linear(self.hidden_size, self.hidden_size)
 
 
     def forward(self, batch):
         """Forward pass of the layer."""
         return self.func()(self.nn(batch) + mean(batch, dim=1).unsqueeze(1))
+
+    def linear_component(self, batch):
+        """Linear component of the layer."""
+        return self.nn(batch)
