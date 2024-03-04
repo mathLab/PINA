5453e83f192fb6a4fdea0bdbc3e7469d5b538a26
cyberguli <guglielmo.padula@aruba.it>
2024-03-04 11:36:32 +0100

    fixed typo

--- pina/model/layers/__init__.py
+++ pina/model/layers/__init__.py
@@ -23,4 +23,4 @@ from .spectral import (
 from .fourier import FourierBlock1D, FourierBlock2D, FourierBlock3D
 from .pod import PODBlock
 from .embedding import PeriodicBoundaryEmbedding
-from .avno_block import AVNOBlock
+from .avno_layer import AVNOBlock
