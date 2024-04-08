64eb7316a32e74d8eaf8ed7baa3e1f00c56ccbb6
ndem0 <ndem0@users.noreply.github.com>
2024-04-08 15:18:17 +0000

    :art: Format Python code with psf/black

--- pina/adaptive_functions/__init__.py
+++ pina/adaptive_functions/__init__.py
@@ -1,21 +1,31 @@
 __all__ = [
-    'AdaptiveActivationFunctionInterface',
-    'AdaptiveReLU',
-    'AdaptiveSigmoid',
-    'AdaptiveTanh',
-    'AdaptiveSiLU',
-    'AdaptiveMish',
-    'AdaptiveELU',
-    'AdaptiveCELU',
-    'AdaptiveGELU',
-    'AdaptiveSoftmin',
-    'AdaptiveSoftmax',
-    'AdaptiveSIREN',
-    'AdaptiveExp']
+    "AdaptiveActivationFunctionInterface",
+    "AdaptiveReLU",
+    "AdaptiveSigmoid",
+    "AdaptiveTanh",
+    "AdaptiveSiLU",
+    "AdaptiveMish",
+    "AdaptiveELU",
+    "AdaptiveCELU",
+    "AdaptiveGELU",
+    "AdaptiveSoftmin",
+    "AdaptiveSoftmax",
+    "AdaptiveSIREN",
+    "AdaptiveExp",
+]
 
-from .adaptive_func import (AdaptiveReLU, AdaptiveSigmoid, AdaptiveTanh,
-                            AdaptiveSiLU, AdaptiveMish, AdaptiveELU,
-                            AdaptiveCELU, AdaptiveGELU, AdaptiveSoftmin,
-                            AdaptiveSoftmax, AdaptiveSIREN, AdaptiveExp)
+from .adaptive_func import (
+    AdaptiveReLU,
+    AdaptiveSigmoid,
+    AdaptiveTanh,
+    AdaptiveSiLU,
+    AdaptiveMish,
+    AdaptiveELU,
+    AdaptiveCELU,
+    AdaptiveGELU,
+    AdaptiveSoftmin,
+    AdaptiveSoftmax,
+    AdaptiveSIREN,
+    AdaptiveExp,
+)
 from .adaptive_func_interface import AdaptiveActivationFunctionInterface
-
