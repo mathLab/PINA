fbefda41131c0f056561e178500bb271c7aa4fdb
valc89 <valentinocarmine9@gmail.com>
2024-04-24 13:24:54 +0200

    SaPinn testing

--- tests/test_solvers/test_sapinn.py
+++ tests/test_solvers/test_sapinn.py
@@ -11,70 +11,59 @@ from pina.model import FeedForward
 from pina.equation.equation import Equation
 from pina.equation.equation_factory import FixedValue
 
-"""def laplace_equation(input_, output_):
-    force_term = (torch.sin(input_.extract(['x']) * torch.pi) *
-                torch.sin(input_.extract(['y']) * torch.pi))
-    delta_u = laplacian(output_.extract(['u']), input_)
-    return delta_u - force_term
+class Burgers(TimeDependentProblem, SpatialProblem):
 
-my_laplace = Equation(laplace_equation)"""
+    # define the burger equation
+    def burger_equation(input_, output_):
+        du = grad(output_, input_)
+        ddu = grad(du, input_, components=['dudx'])
+        return (
+            du.extract(['dudt']) +
+            output_.extract(['u'])*du.extract(['dudx']) -
+            (0.01/torch.pi)*ddu.extract(['ddudxdx'])
+        )
 
-class Burgers(SpatialProblem,TimeDependentProblem):
+    # define initial condition
+    def initial_condition(input_, output_):
+        u_expected = -torch.sin(torch.pi*input_.extract(['x']))
+        return output_.extract(['u']) - u_expected
+
+    # assign output/ spatial and temporal variables
     output_variables = ['u']
     spatial_domain = CartesianDomain({'x': [-1, 1]})
-    temporal_domain = CartesianDomain({'t' : [0, 1]})
+    temporal_domain = CartesianDomain({'t': [0, 1]})
 
-    def initial_condition(input_, output_):
-        u_expected = - torch.sin(torch.pi * input_.extract('x'))
-        return output_.extract('u') - u_expected
-
-    def burger_equation(input_, output_):
-        u_t = grad(output_, input_, components=['u'], d=['t'])
-        u_x = grad(output_, input_, components=['u'], d=['x'])
-        u_xx = laplacian(output_, input_, components=['u'], d=['x'])
-        return u_t + output_.extract('u') * u_x - (0.01/torch.pi) * u_xx
-
+    # problem condition statement
     conditions = {
-        'gamma1': Condition(
-            location=CartesianDomain({'x': -1, 't':  [0, 1]}),
-            equation=FixedValue(0.0)
-        ),
-        'gamma2': Condition(
-            location=CartesianDomain({'x': 1, 't':  [0, 1]}),
-            equation=FixedValue(0.0)
-        ),
-        't0' : Condition(
-            location=CartesianDomain({'x' : [-1, 1], 't' : 0}),
-            equation=Equation(initial_condition)
-        ),
-        'D': Condition(
-            location=CartesianDomain({'x' : [-1, 1], 't' : [0, 1]}),
-            equation=Equation(burger_equation))
+        'gamma1': Condition(location=CartesianDomain({'x': -1, 't': [0, 1]}), equation=FixedValue(0.)),
+        'gamma2': Condition(location=CartesianDomain({'x':  1, 't': [0, 1]}), equation=FixedValue(0.)),
+        't0': Condition(location=CartesianDomain({'x': [-1, 1], 't': 0}), equation=Equation(initial_condition)),
+        'D': Condition(location=CartesianDomain({'x': [-1, 1], 't': [0, 1]}), equation=Equation(burger_equation)),
     }
 
 
 problem = Burgers()
-# Discretizzazione del dominio
+# discretizztion of the domain
 problem.discretise_domain(10000, 'random', locations=['D'])
 problem.discretise_domain(100, 'random', locations=['t0'])
 problem.discretise_domain(200, 'random', locations=['gamma1', 'gamma2'])
 
-# Definizione del modello risolutivo
+# definining the model
 model = FeedForward(
-    layers=[20, 20],
+    layers=[20, 20, 20, 20, 20, 20, 20, 20],
     func=torch.nn.Tanh,
     output_dimensions=len(problem.output_variables),
     input_dimensions=len(problem.input_variables)
 )
 
-# Inizializzazione SAPINN()
+# inizialize sapinn
 sapinn = SAPINN(
     problem,
     model
 )
 
 
-# Creaimo il trainer
+# Cration of the trainer
 trainer = Trainer(
 solver=sapinn,
 max_epochs=10000,
@@ -82,13 +71,14 @@ accelerator='cpu',
 enable_model_summary=False
 )
 
-
+# training
 trainer.train()
 
+# plot of the approximate solution
 pl = Plotter()
-
 pl.plot(sapinn)
 
+# plot of weights
 for key, value in sapinn.models[1].torchmodel.items():
     plt.scatter(problem.input_pts[key].extract('t').tensor.detach().numpy(), problem.input_pts[key].extract('x').tensor.detach().numpy(), c=value.forward().detach().numpy())
 plt.colorbar()
