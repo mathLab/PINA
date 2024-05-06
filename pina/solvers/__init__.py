af1fd36970ba499563cafb8913acb7ab61f9467e
Nicola Demo <demo.nicola@gmail.com>
2024-05-06 15:57:48 +0200

    Create __init__.py

--- pina/solvers/__init__.py
+++ pina/solvers/__init__.py
@@ -12,12 +12,7 @@ __all__ = [
     ]
 
 from .solver import SolverInterface
-from .pinns.basepinn import PINNInterface
-from .pinns.pinn import PINN
-from .pinns.gpinn import GPINN
-from .pinns.causalpinn import CausalPINN
-from .pinns.competitive_pinn import CompetitivePINN
-from .pinns.sapinn import SAPINN
+from .pinns import *
 from .supervised import SupervisedSolver
 from .rom import ReducedOrderModelSolver
 from .garom import GAROM
