84f44798807c167cd8965d549a480d6ab40bd1d6
Nicola Demo <demo.nicola@gmail.com>
2024-06-21 14:37:55 +0200

    optimizer and scheduler classes

--- pina/__init__.py
+++ pina/__init__.py
@@ -6,13 +6,17 @@ __all__ = [
     "Condition",
     "SamplePointDataset",
     "SamplePointLoader",
+    "TorchOptimizer",
+    "TorchScheduler",
 ]
 
 from .meta import *
-#from .label_tensor import LabelTensor
+from .label_tensor import LabelTensor
 from .solvers.solver import SolverInterface
 from .trainer import Trainer
 from .plotter import Plotter
 from .condition import Condition
 from .dataset import SamplePointDataset
 from .dataset import SamplePointLoader
+from .optimizer import TorchOptimizer
+from .scheduler import TorchScheduler
