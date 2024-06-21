eec0249c91eb14c7c12d83b9d9751a75c655f0d2
Nicola Demo <demo.nicola@gmail.com>
2024-06-21 14:37:55 +0200

    optimizer and scheduler classes

---
+++ tests/test_optimizer.py
@@ -0,0 +1,20 @@
+
+import torch
+import pytest
+from pina import TorchOptimizer
+
+opt_list = [
+    torch.optim.Adam,
+    torch.optim.AdamW,
+    torch.optim.SGD,
+    torch.optim.RMSprop
+]
+
+@pytest.mark.parametrize("optimizer_class", opt_list)
+def test_constructor(optimizer_class):
+    TorchOptimizer(optimizer_class, lr=1e-3)
+
+@pytest.mark.parametrize("optimizer_class", opt_list)
+def test_hook(optimizer_class):
+    opt = TorchOptimizer(optimizer_class, lr=1e-3)
+    opt.hook(torch.nn.Linear(10, 10).parameters())
