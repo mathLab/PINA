863c19af573da46159baf94e9ac265c0d7d23093
Anna Ivagnes <s274001@studenti.polito.it>
2024-07-29 11:38:02 +0200

    minor fix

--- pina/model/layers/rbf_layer.py
+++ pina/model/layers/rbf_layer.py
@@ -1,3 +1,5 @@
+"""Module for Radial Basis Function Interpolation layer."""
+
 import math
 import warnings
 from itertools import combinations_with_replacement
@@ -53,7 +55,7 @@ def gaussian(r):
     '''
     return torch.exp(-r**2)
 
-RADIAL_FUNCS = {
+radial_functions = {
    "linear": linear,
    "thin_plate_spline": thin_plate_spline,
    "cubic": cubic,
@@ -64,9 +66,9 @@ RADIAL_FUNCS = {
    "gaussian": gaussian
    }
 
-SCALE_INVARIANT = {"linear", "thin_plate_spline", "cubic", "quintic"}
+scale_invariant = {"linear", "thin_plate_spline", "cubic", "quintic"}
 
-MIN_DEGREE = {
+min_degree_funcs = {
     "multiquadric": 0,
     "linear": 0,
     "thin_plate_spline": 1,
@@ -94,9 +96,8 @@ class RBFLayer(torch.nn.Module):
         epsilon=None,
         degree=None,
     ):
-        super().__init__()
         """
-        :param int | None neighbors: Number of neighbors to use for the
+        :param int neighbors: Number of neighbors to use for the
             interpolation.
             If ``None``, use all data points.
         :param float smoothing: Smoothing parameter for the interpolation.
@@ -105,17 +106,18 @@ class RBFLayer(torch.nn.Module):
             ``linear``, ``thin_plate_spline``, ``cubic``, ``quintic``,
             ``multiquadric``, ``inverse_multiquadric``, ``inverse_quadratic``,
             or ``gaussian``.
-        :param float | None epsilon: Shape parameter that scaled the input to
-            the RBF. This defaults to 1 for kernels in ``SCALE_INVARIANT``
+        :param float epsilon: Shape parameter that scaled the input to
+            the RBF. This defaults to 1 for kernels in ``scale_invariant``
             dictionary, and must be specified for other kernels.
-        :param int | None degree: Degree of the added polynomial.
+        :param int degree: Degree of the added polynomial.
             For some kernels, there exists a minimum degree of the polynomial
             such that the RBF is well-posed. Those minimum degrees are specified
-            in the `MIN_DEGREE` dictionary above. If `degree` is less than the
-            minimum degree, a warning is raised and the degree is set to the
+            in the `min_degree_funcs` dictionary above. If `degree` is less than
+            the minimum degree, a warning is raised and the degree is set to the
             minimum value.
         """
 
+        super().__init__()
         check_consistency(neighbors, (int, type(None)))
         check_consistency(smoothing, (int, float, torch.Tensor))
         check_consistency(kernel, str)
@@ -160,7 +162,7 @@ class RBFLayer(torch.nn.Module):
 
     @kernel.setter
     def kernel(self, value):
-        if value not in RADIAL_FUNCS:
+        if value not in radial_functions:
             raise ValueError(f"Unknown kernel: {value}")
         self._kernel = value.lower()
 
@@ -176,7 +178,7 @@ class RBFLayer(torch.nn.Module):
     @epsilon.setter
     def epsilon(self, value):
         if value is None:
-            if self.kernel in SCALE_INVARIANT:
+            if self.kernel in scale_invariant:
                 value = 1.0
             else:
                 raise ValueError("Must specify `epsilon` for this kernel.")
@@ -195,14 +197,14 @@ class RBFLayer(torch.nn.Module):
 
     @degree.setter
     def degree(self, value):
-        min_degree = MIN_DEGREE.get(self.kernel, -1)
+        min_degree = min_degree_funcs.get(self.kernel, -1)
         if value is None:
             value = max(min_degree, 0)
         else:
             value = int(value)
             if value < -1:
                 raise ValueError("`degree` must be at least -1.")
-            elif value < min_degree:
+            if value < min_degree:
                 warnings.warn(
                     "`degree` is too small for this kernel. Setting to "
                     f"{min_degree}.", UserWarning,
@@ -240,7 +242,8 @@ class RBFLayer(torch.nn.Module):
         else:
             raise NotImplementedError("neighbors currently not supported")
 
-        powers = RBFLayer.monomial_powers(self.y.shape[1], self.degree).to(y.device)
+        powers = RBFLayer.monomial_powers(self.y.shape[1], self.degree).to(
+                y.device)
         if powers.shape[0] > nobs:
             raise ValueError("The data is not compatible with the "
                 "requested degree.")
@@ -271,7 +274,7 @@ class RBFLayer(torch.nn.Module):
                 f"{self.y.shape[1]}."
             )
 
-        kernel_func = RADIAL_FUNCS[self.kernel]
+        kernel_func = radial_functions[self.kernel]
 
         yeps = self.y * self.epsilon
         xeps = x * self.epsilon
@@ -364,7 +367,7 @@ class RBFLayer(torch.nn.Module):
         p = d.shape[0]
         s = d.shape[1]
         r = powers.shape[0]
-        kernel_func = RADIAL_FUNCS[kernel]
+        kernel_func = radial_functions[kernel]
 
         mins = torch.min(y, dim=0).values
         maxs = torch.max(y, dim=0).values
@@ -408,15 +411,15 @@ class RBFLayer(torch.nn.Module):
             of the interpolator
         """
 
-        lhs, rhs, shift, scale = RBFLayer.build(y, d, smoothing, kernel, epsilon,
-                powers)
+        lhs, rhs, shift, scale = RBFLayer.build(y, d, smoothing, kernel,
+                epsilon, powers)
         try:
             coeffs = torch.linalg.solve(lhs, rhs)
         except RuntimeError as e:
             msg = "Singular matrix."
             nmonos = powers.shape[0]
             if nmonos > 0:
-                pmat = polynomial_matrix((y - shift) / scale, powers)
+                pmat = RBFLayer.polynomial_matrix((y - shift) / scale, powers)
                 rank = torch.linalg.matrix_rank(pmat)
                 if rank < nmonos:
                     msg = (
