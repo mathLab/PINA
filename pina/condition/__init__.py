7a1ecbf90b89a94a190f405611a15ec7b7696d78
Nicola Demo <demo.nicola@gmail.com>
2024-08-05 17:34:34 +0200

    refact

---
+++ pina/condition/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'Condition',
+    'ConditionInterface',
+    'InputOutputCondition',
+    'InputEquationCondition'
+    'LocationEquationCondition',
+]
+
+from .condition_interface import ConditionInterface
+from .input_output_condition import InputOutputCondition
