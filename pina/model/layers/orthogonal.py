81887b927727ec6c7b6c76bdfe6029f522a6c805
Nicola Demo <demo.nicola@gmail.com>
2024-08-26 11:26:31 +0200

    Update orthogonal.py

--- pina/model/layers/orthogonal.py
+++ pina/model/layers/orthogonal.py
@@ -32,15 +32,17 @@ class OrthogonalBlock(torch.nn.Module):
         # check dim is less than all the other dimensions
         if X.shape[self.dim] > min(X.shape):
             raise Warning(
-                "The dimension where to orthogonalize is greater\
-                          than the other dimensions"
+                "The dimension where to orthogonalize is greater"
+                " than the other dimensions"
             )
 
         result = torch.zeros_like(X)
+        
         # normalize first basis
         X_0 = torch.select(X, self.dim, 0)
         result_0 = torch.select(result, self.dim, 0)
         result_0 += X_0 / torch.norm(X_0)
+        
         # iterate over the rest of the basis with Gram-Schmidt
         for i in range(1, X.shape[self.dim]):
             v = torch.select(X, self.dim, i)
@@ -52,4 +54,5 @@ class OrthogonalBlock(torch.nn.Module):
                 ) * torch.select(result, self.dim, j)
             result_i = torch.select(result, self.dim, i)
             result_i += v / torch.norm(v)
+            
         return result
