a1e041c1f0cdd93fe6e59c676c0556ce2f9d76a3
ndem0 <ndem0@users.noreply.github.com>
2024-08-26 08:49:18 +0000

    :art: Format Python code with psf/black

--- pina/model/layers/orthogonal.py
+++ pina/model/layers/orthogonal.py
@@ -9,6 +9,7 @@ class OrthogonalBlock(torch.nn.Module):
     The module takes a tensor of size [N, M] and returns a tensor of
     size [N, M] where the columns are orthonormal.
     """
+
     def __init__(self, dim=-1):
         """
         Initialize the OrthogonalBlock module.
@@ -30,21 +31,25 @@ class OrthogonalBlock(torch.nn.Module):
         """
         # check dim is less than all the other dimensions
         if X.shape[self.dim] > min(X.shape):
-            raise Warning("The dimension where to orthogonalize is greater\
-                          than the other dimensions")
+            raise Warning(
+                "The dimension where to orthogonalize is greater\
+                          than the other dimensions"
+            )
 
         result = torch.zeros_like(X)
         # normalize first basis
         X_0 = torch.select(X, self.dim, 0)
         result_0 = torch.select(result, self.dim, 0)
-        result_0 += X_0/torch.norm(X_0)
+        result_0 += X_0 / torch.norm(X_0)
         # iterate over the rest of the basis with Gram-Schmidt
         for i in range(1, X.shape[self.dim]):
             v = torch.select(X, self.dim, i)
             for j in range(i):
-                v -= torch.sum(v * torch.select(result, self.dim, j),
-                               dim=self.dim, keepdim=True) * torch.select(
-                                   result, self.dim, j)
+                v -= torch.sum(
+                    v * torch.select(result, self.dim, j),
+                    dim=self.dim,
+                    keepdim=True,
+                ) * torch.select(result, self.dim, j)
             result_i = torch.select(result, self.dim, i)
-            result_i += v/torch.norm(v)
+            result_i += v / torch.norm(v)
         return result
