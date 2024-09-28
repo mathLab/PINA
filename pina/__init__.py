40c4a078e6096913837781cc451c5644ebb0f234
FilippoOlivo <filippo@filippoolivo.com>
2024-09-28 12:22:03 +0200

    Update __init__.py in pina folder

--- pina/__init__.py
+++ pina/__init__.py
@@ -6,8 +6,6 @@ __all__ = [
     "Condition",
     "SamplePointDataset",
     "SamplePointLoader",
-    "TorchOptimizer",
-    "TorchScheduler",
 ]
 
 from .meta import *
@@ -15,8 +13,6 @@ from .label_tensor import LabelTensor
 from .solvers.solver import SolverInterface
 from .trainer import Trainer
 from .plotter import Plotter
-from .optimizer import TorchOptimizer
-from .scheduler import TorchScheduler
 from .condition.condition import Condition
 from .data import SamplePointDataset
 from .data import SamplePointLoader
