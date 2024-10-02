d118643bd140481dcc1054d12c45d1f224b8b170
FilippoOlivo <filippo@filippoolivo.com>
2024-10-02 17:07:54 +0200

    Add summation and remove deepcopy (only for tensors) in LabelTensor class

--- tests/test_label_tensor.py
+++ tests/test_label_tensor.py
@@ -17,9 +17,10 @@ labels_row = {
         "dof": range(20)
     }
 }
+labels_list = ['x', 'y', 'z']
 labels_all = labels_column | labels_row
 
-@pytest.mark.parametrize("labels", [labels_column, labels_row, labels_all])
+@pytest.mark.parametrize("labels", [labels_column, labels_row, labels_all, labels_list])
 def test_constructor(labels):
     LabelTensor(data, labels)
 
@@ -61,7 +62,6 @@ def test_extract_2D(labels_te):
     assert torch.all(torch.isclose(data[2,2].reshape(1, 1), new))
 
 def test_extract_3D():
-    labels = labels_all
     data = torch.rand(20, 3, 4)
     labels = {
         1: {
@@ -80,6 +80,7 @@ def test_extract_3D():
 
     tensor = LabelTensor(data, labels)
     new = tensor.extract(labels_te)
+    tensor2 = LabelTensor(data, labels)
     assert new.ndim == tensor.ndim
     assert new.shape[0] == 20
     assert new.shape[1] == 2
@@ -88,6 +89,10 @@ def test_extract_3D():
         data[:, 0::2, 1:4].reshape(20, 2, 3),
         new
     ))
+    assert tensor2.ndim == tensor.ndim
+    assert tensor2.shape == tensor.shape
+    assert tensor.labels == tensor2.labels
+    assert new.shape != tensor.shape
 
 def test_concatenation_3D():
     data_1 = torch.rand(20, 3, 4)
@@ -146,3 +151,29 @@ def test_concatenation_3D():
     assert lt_cat.labels[2]['dof'] == range(5)
     assert lt_cat.labels[0]['dof'] == range(20)
     assert lt_cat.labels[1]['dof'] == range(3)
+
+
+def test_summation():
+    lt1 = LabelTensor(torch.ones(20,3), labels_all)
+    lt2 = LabelTensor(torch.ones(30,3), ['x', 'y', 'z'])
+    with pytest.raises(RuntimeError):
+        LabelTensor.summation([lt1, lt2])
+    lt1 = LabelTensor(torch.ones(20,3), labels_all)
+    lt2 = LabelTensor(torch.ones(20,3), labels_all)
+    lt_sum = LabelTensor.summation([lt1, lt2])
+    assert lt_sum.ndim == lt_sum.ndim
+    assert lt_sum.shape[0] == 20
+    assert lt_sum.shape[1] == 3
+    assert lt_sum.labels == labels_all
+    assert torch.eq(lt_sum.tensor, torch.ones(20,3)*2).all()
+    lt1 = LabelTensor(torch.ones(20,3), labels_all)
+    lt2 = LabelTensor(torch.ones(20,3), labels_all)
+    lt3 = LabelTensor(torch.zeros(20, 3), labels_all)
+    lt_sum = LabelTensor.summation([lt1, lt2, lt3])
+    assert lt_sum.ndim == lt_sum.ndim
+    assert lt_sum.shape[0] == 20
+    assert lt_sum.shape[1] == 3
+    assert lt_sum.labels == labels_all
+    assert torch.eq(lt_sum.tensor, torch.ones(20,3)*2).all()
+
+
