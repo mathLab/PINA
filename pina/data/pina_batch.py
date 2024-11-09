dfc6bf8d9e45af6aa19f9a27357f092a911ef02d
FilippoOlivo <filippo@filippoolivo.com>
2024-11-09 14:49:16 +0100

    Improve PinaBatch

--- pina/data/pina_batch.py
+++ pina/data/pina_batch.py
@@ -1,6 +1,8 @@
 """
 Batch management module
 """
+from attr import attributes
+
 from .pina_subset import PinaSubset
 
 
@@ -13,11 +15,16 @@ class Batch:
     def __init__(self, dataset_dict, idx_dict, require_grad=True):
         self.attributes = []
         for k, v in dataset_dict.items():
-            setattr(self, k, v)
+            index = idx_dict[k]
+            if isinstance(v, PinaSubset):
+                dataset_index = v.indices
+                if isinstance(dataset_index, slice):
+                    index = slice(dataset_index.start + index.start,
+                                  min(dataset_index.start + index.stop,
+                                      dataset_index.stop))
+            setattr(self, k, PinaSubset(v.dataset, index,
+                                       require_grad=require_grad))
             self.attributes.append(k)
-
-        for k, v in idx_dict.items():
-            setattr(self, k + '_idx', v)
         self.require_grad = require_grad
 
     def __len__(self):
@@ -31,20 +38,6 @@ class Batch:
             attribute = getattr(self, dataset)
             length += len(attribute)
         return length
-
-    def __getattribute__(self, item):
-        if item in super().__getattribute__('attributes'):
-            dataset = super().__getattribute__(item)
-            index = super().__getattribute__(item + '_idx')
-            if isinstance(dataset, PinaSubset):
-                dataset_index = dataset.indices
-                if isinstance(dataset_index, slice):
-                    index = slice(dataset_index.start + index.start,
-                                  min(dataset_index.start + index.stop,
-                                      dataset_index.stop))
-            return PinaSubset(dataset.dataset, index,
-                              require_grad=self.require_grad)
-        return super().__getattribute__(item)
 
     def __getattr__(self, item):
         if item == 'data' and len(self.attributes) == 1:
