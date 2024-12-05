7b1d54d40dd301c108aa28e1632b0fa2d2b19eaa
FilippoOlivo <filippo@filippoolivo.com>
2024-12-05 19:07:57 +0100

    Refactor Graph class

--- pina/graph.py
+++ pina/graph.py
@@ -1,118 +1,62 @@
-""" Module for Loss class """
-
-import logging
-from torch_geometric.nn import MessagePassing, InstanceNorm, radius_graph
-from torch_geometric.data import Data
 import torch
+from . import LabelTensor
+from torch_geometric.nn import radius_graph, knn_graph
+from torch_geometric.data import Data
+
 
 class Graph:
-    """
-    PINA Graph managing the PyG Data class.
-    """
-    def __init__(self, data):
-        self.data = data
-            
-    @staticmethod
-    def _build_triangulation(**kwargs):
-        logging.debug("Creating graph with triangulation mode.")
+    def __init__(self, x=None, pos=None, edge_index=None, edge_attr=None,
+                 build_edge_attr=False, **kwargs):
+        if edge_index is None:
+            if isinstance(pos, (torch.Tensor, LabelTensor)):
+                edge_index = self._build_edge_index(pos, **kwargs)
+                if build_edge_attr:
+                    edge_attr = self._build_edge_attr(pos, edge_index)
+            else:
+                edge_index = [self._build_edge_index(p, **kwargs) for p in pos]
+                if build_edge_attr:
+                    edge_attr = [self._build_edge_attr(p, ei) for p, ei in
+                                 zip(pos, edge_index)]
 
-        # check for mandatory arguments
-        if "nodes_coordinates" not in kwargs:
-            raise ValueError("Nodes coordinates must be provided in the kwargs.")
-        if "nodes_data" not in kwargs:
-            raise ValueError("Nodes data must be provided in the kwargs.")
-        if "triangles" not in kwargs:
-            raise ValueError("Triangles must be provided in the kwargs.")
-
-        nodes_coordinates = kwargs["nodes_coordinates"]
-        nodes_data = kwargs["nodes_data"]
-        triangles = kwargs["triangles"]
-
-
-
-        def less_first(a, b):
-            return [a, b] if a < b else [b, a]
-
-        list_of_edges = []
-
-        for triangle in triangles:
-            for e1, e2 in [[0, 1], [1, 2], [2, 0]]:
-                list_of_edges.append(less_first(triangle[e1],triangle[e2]))
-
-        array_of_edges = torch.unique(torch.Tensor(list_of_edges), dim=0) # remove duplicates
-        array_of_edges = array_of_edges.t().contiguous()
-        print(array_of_edges)
-
-        # list_of_lengths = []
-
-        # for p1,p2 in array_of_edges:
-        #     x1, y1 = tri.points[p1]
-        #     x2, y2 = tri.points[p2]
-        #     list_of_lengths.append((x1-x2)**2 + (y1-y2)**2)
-
-        # array_of_lengths = np.sqrt(np.array(list_of_lengths))
-
-        # return array_of_edges, array_of_lengths
-
-        return Data(
-            x=nodes_data,
-            pos=nodes_coordinates.T,
-            
-            edge_index=array_of_edges,
-        )
-
-    @staticmethod
-    def _build_radius(**kwargs):
-        logging.debug("Creating graph with radius mode.")
-
-        # check for mandatory arguments
-        if "nodes_coordinates" not in kwargs:
-            raise ValueError("Nodes coordinates must be provided in the kwargs.")
-        if "nodes_data" not in kwargs:
-            raise ValueError("Nodes data must be provided in the kwargs.")
-        if "radius" not in kwargs:
-            raise ValueError("Radius must be provided in the kwargs.")
-
-        nodes_coordinates = kwargs["nodes_coordinates"]
-        nodes_data = kwargs["nodes_data"]
-        radius = kwargs["radius"]
-
-        edges_data = kwargs.get("edge_data", None) 
-        loop = kwargs.get("loop", False)
-        batch = kwargs.get("batch", None)
-
-        logging.debug(f"radius: {radius}, loop: {loop}, "
-                        f"batch: {batch}")
-
-        edge_index = radius_graph(
-            x=nodes_coordinates.tensor,
-            r=radius,
-            loop=loop,
-            batch=batch,
-        )
-
-        logging.debug(f"edge_index computed")
-        return Data(
-            x=nodes_data.tensor,
-            pos=nodes_coordinates.tensor,
-            edge_index=edge_index,
-            edge_attr=edges_data,
-        )
-
-    @staticmethod
-    def build(mode, **kwargs):
-        """
-        Constructor for the `Graph` class.
-        """
-        if mode == "radius":
-            graph = Graph._build_radius(**kwargs)
-        elif mode == "triangulation":
-            graph = Graph._build_triangulation(**kwargs)
+        if isinstance(x, list) and isinstance(pos, list):
+            pass
+        elif isinstance(x, list) and isinstance(pos,
+                                                (torch.Tensor, LabelTensor)):
+            pos, edge_index = [pos] * len(x), [edge_index] * len(x)
+            if edge_attr is not None:
+                edge_attr = [edge_attr] * len(x)
+        elif isinstance(x, (torch.Tensor, LabelTensor)) and isinstance(pos, (
+                torch.Tensor, LabelTensor)):
+            x, pos, edge_index = [x], [pos], [edge_index]
+            if edge_attr is not None:
+                edge_attr = [edge_attr]
+        elif isinstance(x, (torch.Tensor, LabelTensor)) and isinstance(pos,
+                                                                       list):
+            x = [x] * len(pos)
         else:
-            raise ValueError(f"Mode {mode} not recognized")
-        
-        return Graph(graph)
+            raise ValueError(
+                "The input must be either x and pos or edge_index.")
 
+        self.data = []
+        self.build_graph_list(x, pos, edge_index, edge_attr)
 
-    def __repr__(self):
-        return f"Graph(data={self.data})"
+    def build_graph_list(self, x, pos, edge_index, edge_attr):
+        for i, (x_, pos_, edge_index_) in enumerate(zip(x, pos, edge_index)):
+            if edge_attr is not None:
+                self.data.append(Data(x=x_, pos=pos_, edge_index=edge_index_,
+                                      edge_attr=edge_attr[i]))
+            else:
+                self.data.append(Data(x=x_, pos=pos_, edge_index=edge_index_, ))
+
+    @staticmethod
+    def _build_edge_index(pos, method, **kwargs):
+        if method == 'radius':
+            return radius_graph(pos, **kwargs)
+        elif method == 'knn':
+            return knn_graph(pos, **kwargs)
+        else:
+            raise ValueError("The method must be 'radius'.")
+
+    @staticmethod
+    def _build_edge_attr(pos, edge_index, ):
+        return torch.norm((pos[edge_index[0]] - pos[edge_index[1]]), dim=-1)
