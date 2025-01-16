0e7e6602235f0c3a56f73110d168c57313501507
Nicola Demo <demo.nicola@gmail.com>
2025-01-16 19:03:18 +0100

    fix some tests

--- tests/test_callbacks/test_progress_bar.py
+++ tests/test_callbacks/test_progress_bar.py
@@ -1,59 +1,13 @@
-import torch
-import pytest
-
-from pina.problem import SpatialProblem
-from pina.operators import laplacian
-from pina.geometry import CartesianDomain
-from pina import Condition, LabelTensor
 from pina.solvers import PINN
 from pina.trainer import Trainer
 from pina.model import FeedForward
-from pina.equation.equation import Equation
-from pina.equation.equation_factory import FixedValue
 from pina.callbacks.processing_callbacks import PINAProgressBar
-
-
-def laplace_equation(input_, output_):
-    force_term = (torch.sin(input_.extract(['x']) * torch.pi) *
-                  torch.sin(input_.extract(['y']) * torch.pi))
-    delta_u = laplacian(output_.extract(['u']), input_)
-    return delta_u - force_term
-
-
-my_laplace = Equation(laplace_equation)
-in_ = LabelTensor(torch.tensor([[0., 1.]]), ['x', 'y'])
-out_ = LabelTensor(torch.tensor([[0.]]), ['u'])
-
-
-class Poisson(SpatialProblem):
-    output_variables = ['u']
-    spatial_domain = CartesianDomain({'x': [0, 1], 'y': [0, 1]})
-
-    conditions = {
-        'gamma1': Condition(
-            location=CartesianDomain({'x': [0, 1], 'y':  1}),
-            equation=FixedValue(0.0)),
-        'gamma2': Condition(
-            location=CartesianDomain({'x': [0, 1], 'y': 0}),
-            equation=FixedValue(0.0)),
-        'gamma3': Condition(
-            location=CartesianDomain({'x':  1, 'y': [0, 1]}),
-            equation=FixedValue(0.0)),
-        'gamma4': Condition(
-            location=CartesianDomain({'x': 0, 'y': [0, 1]}),
-            equation=FixedValue(0.0)),
-        'D': Condition(
-            input_points=LabelTensor(torch.rand(size=(100, 2)), ['x', 'y']),
-            equation=my_laplace),
-        'data': Condition(
-            input_points=in_,
-            output_points=out_)
-    }
+from pina.problem.zoo import Poisson2DSquareProblem as Poisson
 
 
 # make the problem
 poisson_problem = Poisson()
-boundaries = ['gamma1', 'gamma2', 'gamma3', 'gamma4']
+boundaries = ['nil_g1', 'nil_g2', 'nil_g3', 'nil_g4']
 n = 10
 poisson_problem.discretise_domain(n, 'grid', locations=boundaries)
 model = FeedForward(len(poisson_problem.input_variables),
