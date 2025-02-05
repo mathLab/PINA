85a4e8eeaee8412a2306b37ffede5bda5b66f202
giovanni <giovanni.canali98@yahoo.it>
2025-02-05 10:48:28 +0100

    added problems to zoo

--- pina/problem/zoo/__init__.py
+++ pina/problem/zoo/__init__.py
@@ -1,5 +1,9 @@
 __all__ = [
-    'Poisson2DSquareProblem'
+    'Poisson2DSquareProblem',
+    'DiffusionReactionProblem',
+    'InverseDiffusionReactionProblem'
 ]
 
 from .poisson_2d_square import Poisson2DSquareProblem
+from .diffusion_reaction import DiffusionReactionProblem
+from .inverse_diffusion_reaction import InverseDiffusionReactionProblem
---
+++ pina/problem/zoo/inverse_diffusion_reaction.py
@@ -0,0 +1,44 @@
+""" Definition of the diffusion-reaction problem."""
+
+import torch
+from pina import Condition, LabelTensor
+from pina.problem import SpatialProblem, TimeDependentProblem, InverseProblem
+from pina.equation.equation import Equation
+from pina.domain import CartesianDomain
+from pina.operators import grad
+
+def diffusion_reaction(input_, output_):
+    x = input_.extract('x')
+    t = input_.extract('t')
+    u_t = grad(output_, input_, d='t')
+    u_x = grad(output_, input_, d='x')
+    u_xx = grad(u_x, input_, d='x')
+    r = torch.exp(-t) * (1.5 * torch.sin(2*x) + (8/3) * torch.sin(3*x) +
+                         (15/4) * torch.sin(4*x) + (63/8) * torch.sin(8*x))
+    return u_t - u_xx - r
+
+class InverseDiffusionReactionProblem(TimeDependentProblem,
+                                      SpatialProblem,
+                                      InverseProblem):
+    output_variables = ['u']
+    spatial_domain = CartesianDomain({'x': [-torch.pi, torch.pi]})
+    temporal_domain = CartesianDomain({'t': [0, 1]})
+    unknown_parameter_domain = CartesianDomain({'mu': [-1, 1]})
+
+    conditions = {
+        'D': Condition(
+            domain=CartesianDomain({'x': [-torch.pi, torch.pi], 't': [0, 1]}),
+            equation=Equation(diffusion_reaction)),
+        'data' : Condition(
+            input_points=LabelTensor(torch.tensor([[0., 0.]]), ['x', 't']),
+            output_points=LabelTensor(torch.tensor([[0.]]), ['u'])),
+    }
+
+    def _solution(self, pts):
+        t = pts.extract('t')
+        x = pts.extract('x')
+        return torch.exp(-t) * (
+            torch.sin(x) + (1/2)*torch.sin(2*x) + (1/3)*torch.sin(3*x) +
+            (1/4)*torch.sin(4*x) + (1/8)*torch.sin(8*x)
+        )
+    
