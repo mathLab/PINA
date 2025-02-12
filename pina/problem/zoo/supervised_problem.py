87f94c5b410577614c9dd5a1853a4117c6a0f25d
FilippoOlivo <filippo@filippoolivo.com>
2025-02-12 20:09:19 +0100

    Add comments

--- pina/problem/zoo/supervised_problem.py
+++ pina/problem/zoo/supervised_problem.py
@@ -3,13 +3,35 @@ from pina import Condition
 from pina import Graph
 
 class SupervisedProblem(AbstractProblem):
-    conditions = {}
+    """
+    A problem definition for supervised learning in PINA.
+
+    This class allows an easy and straightforward definition of a Supervised problem,
+    based on a single condition of type `InputOutputPointsCondition`
+
+    Examples
+    --------
+    >>> import torch
+    >>> input_data = torch.rand((100, 10))
+    >>> output_data = torch.rand((100, 10))
+    >>> problem = SupervisedProblem(input_data, output_data)
+    """
+    conditions = dict()
     output_variables = None
 
     def __init__(self, input_, output_):
+        """
+        Initialize the SupervisedProblem class
+
+        :param input_: Input data of the problem
+        :type input_: torch.Tensor | Graph
+        :param output_: Output data of the problem
+        :type output_: torch.Tensor
+        """
         if isinstance(input_, Graph):
             input_ = input_.data
         self.conditions['data'] = Condition(
             input_points=input_,
             output_points = output_
         )
+        super().__init__()
