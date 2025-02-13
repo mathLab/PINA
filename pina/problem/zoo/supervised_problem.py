5eaac69d6c1caf1ba30edc9d10979ea680fc0014
FilippoOlivo <filippo@filippoolivo.com>
2025-02-13 11:29:37 +0100

    Modify docstring

--- pina/problem/zoo/supervised_problem.py
+++ pina/problem/zoo/supervised_problem.py
@@ -9,12 +9,11 @@ class SupervisedProblem(AbstractProblem):
     This class allows an easy and straightforward definition of a Supervised problem,
     based on a single condition of type `InputOutputPointsCondition`
 
-    Examples
-    --------
-    >>> import torch
-    >>> input_data = torch.rand((100, 10))
-    >>> output_data = torch.rand((100, 10))
-    >>> problem = SupervisedProblem(input_data, output_data)
+    :Example:
+        >>> import torch
+        >>> input_data = torch.rand((100, 10))
+        >>> output_data = torch.rand((100, 10))
+        >>> problem = SupervisedProblem(input_data, output_data)
     """
     conditions = dict()
     output_variables = None
