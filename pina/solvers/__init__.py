886bd23fdb7fe407ad6b2ad62a811c1fa1f2c390
Dario Coscia <dariocos99@gmail.com>
2025-02-19 16:29:04 +0100

    Back compatibility 0.1

--- pina/solvers/__init__.py
+++ pina/solvers/__init__.py
@@ -1,13 +1,13 @@
 import warnings
 
-from ..block import *
-from ...utils import custom_warning_format
+from ..solver import *
+from ..utils import custom_warning_format
 
 # back-compatibility 0.1
 # Set the custom format for warnings
 warnings.formatwarning = custom_warning_format
 warnings.filterwarnings("always", category=DeprecationWarning)
 warnings.warn(
-            f"'pina.model.layers' is deprecated and will be removed "
-            f"in future versions. Please use 'pina.model.block' instead.",
+            f"'pina.solvers' is deprecated and will be removed "
+            f"in future versions. Please use 'pina.solver' instead.",
             DeprecationWarning)
