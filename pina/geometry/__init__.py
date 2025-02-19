6da9863daee6ecc9c584589dec9f1f1cbb4d1088
Dario Coscia <dariocos99@gmail.com>
2025-02-19 16:29:04 +0100

    Back compatibility 0.1

---
+++ pina/geometry/__init__.py
@@ -0,0 +1,17 @@
+import warnings
+
+from ..domain import *
+from ..utils import custom_warning_format
+
+# back-compatibility 0.1
+# creating alias
+Location = DomainInterface
+
+# Set the custom format for warnings
+warnings.formatwarning = custom_warning_format
+warnings.filterwarnings("always", category=DeprecationWarning)
+warnings.warn(
+            "'pina.geometry' is deprecated and will be removed "
+            "in future versions. Please use 'pina.domain' instead. "
+            "Location moved to DomainInferface object.",
+            DeprecationWarning)
--- pina/solvers/pinns/__init__.py
+++ pina/solvers/pinns/__init__.py
@@ -1,6 +1,6 @@
 import warnings
 
-from ..block import *
+from ...solver.physic_informed_solver import *
 from ...utils import custom_warning_format
 
 # back-compatibility 0.1
@@ -8,6 +8,7 @@ from ...utils import custom_warning_format
 warnings.formatwarning = custom_warning_format
 warnings.filterwarnings("always", category=DeprecationWarning)
 warnings.warn(
-            f"'pina.model.layers' is deprecated and will be removed "
-            f"in future versions. Please use 'pina.model.block' instead.",
+            "'pina.solvers.pinns' is deprecated and will be removed "
+            "in future versions. Please use "
+            "'pina.solver.physic_informed_solver' instead.",
             DeprecationWarning)
