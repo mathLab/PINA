cf0f7e8778790b52d13009c9fcbf577b8a6f0a89
FilippoOlivo <filippo@filippoolivo.com>
2025-02-23 12:19:47 +0100

    Add length checks in Graph and tests for LabelTensor graphs

--- tests/test_graph/test_graph.py
+++ tests/test_graph/test_graph.py
@@ -6,22 +6,21 @@ from pina.graph import RadiusGraph, KNNGraph
 @pytest.mark.parametrize(
     "x, pos",
     [
-        ([torch.rand(10, 2) for _ in range(3)],
-         [torch.rand(10, 3) for _ in range(3)]),
-        ([torch.rand(10, 2) for _ in range(3)],
-         [torch.rand(10, 3) for _ in range(3)]),
+        (
+            [torch.rand(10, 2) for _ in range(3)],
+            [torch.rand(10, 3) for _ in range(3)],
+        ),
         (torch.rand(3, 10, 2), torch.rand(3, 10, 3)),
-        (torch.rand(3, 10, 2), torch.rand(3, 10, 3)),
-    ]
+    ],
 )
 def test_build_multiple_graph_multiple_val(x, pos):
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=0.3)
     assert len(graph.data) == 3
     data = graph.data
     assert all(torch.isclose(d_.x, x_).all() for (d_, x_) in zip(data, x))
     assert all(torch.isclose(d_.pos, pos_).all() for d_, pos_ in zip(data, pos))
     assert all(len(d.edge_index) == 2 for d in data)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=0.3)
     data = graph.data
     assert all(torch.isclose(d_.x, x_).all() for (d_, x_) in zip(data, x))
     assert all(torch.isclose(d_.pos, pos_).all() for d_, pos_ in zip(data, pos))
@@ -38,16 +37,16 @@ def test_build_multiple_graph_multiple_val(x, pos):
     assert all([d.edge_index.shape[1] == d.edge_attr.shape[0]] for d in data)
 
 
-def test_build_single_graph_multiple_val():
+def test_build_single_graph_single_val():
     x = torch.rand(10, 2)
     pos = torch.rand(10, 3)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=0.3)
     assert len(graph.data) == 1
     data = graph.data
     assert all(torch.isclose(d.x, x).all() for d in data)
     assert all(torch.isclose(d_.pos, pos).all() for d_ in data)
     assert all(len(d.edge_index) == 2 for d in data)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=0.3)
     data = graph.data
     assert len(graph.data) == 1
     assert all(torch.isclose(d.x, x).all() for d in data)
@@ -78,20 +77,18 @@ def test_build_single_graph_multiple_val():
     "pos",
     [
         ([torch.rand(10, 3) for _ in range(3)]),
-        ([torch.rand(10, 3) for _ in range(3)]),
         (torch.rand(3, 10, 3)),
-        (torch.rand(3, 10, 3))
-    ]
+    ],
 )
-def test_build_single_graph_single_val(pos):
+def test_build_multi_graph_single_val(pos):
     x = torch.rand(10, 2)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=0.3)
     assert len(graph.data) == 3
     data = graph.data
     assert all(torch.isclose(d.x, x).all() for d in data)
     assert all(torch.isclose(d_.pos, pos_).all() for d_, pos_ in zip(data, pos))
     assert all(len(d.edge_index) == 2 for d in data)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=0.3)
     data = graph.data
     assert all(torch.isclose(d.x, x).all() for d in data)
     assert all(torch.isclose(d_.pos, pos_).all() for d_, pos_ in zip(data, pos))
@@ -114,36 +111,102 @@ def test_build_single_graph_single_val(pos):
     assert all([d.edge_index.shape[1] == d.edge_attr.shape[0]] for d in data)
 
 
+@pytest.mark.parametrize(
+    "x", [([torch.rand(10, 2) for _ in range(3)]), (torch.rand(3, 10, 2))]
+)
+def test_build_single_graph_multi_val(x):
+    pos = torch.rand(10, 3)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=False, r=0.3)
+    assert len(graph.data) == 3
+    data = graph.data
+    assert all(torch.isclose(d.pos, pos).all() for d in data)
+    assert all(torch.isclose(d_.x, x_).all() for d_, x_ in zip(data, x))
+    assert all(len(d.edge_index) == 2 for d in data)
+    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=0.3)
+    data = graph.data
+    assert all(torch.isclose(d.pos, pos).all() for d in data)
+    assert all(torch.isclose(d_.x, x_).all() for d_, x_ in zip(data, x))
+    assert all(len(d.edge_index) == 2 for d in data)
+    assert all(d.edge_attr is not None for d in data)
+    assert all([d.edge_index.shape[1] == d.edge_attr.shape[0]] for d in data)
+
+    graph = KNNGraph(x=x, pos=pos, build_edge_attr=False, k=3)
+    assert len(graph.data) == 3
+    data = graph.data
+    assert all(torch.isclose(d.pos, pos).all() for d in data)
+    assert all(torch.isclose(d_.x, x_).all() for d_, x_ in zip(data, x))
+    assert all(len(d.edge_index) == 2 for d in data)
+
+    graph = KNNGraph(x=x, pos=pos, build_edge_attr=True, k=3)
+    data = graph.data
+    assert all(torch.isclose(d.pos, pos).all() for d in data)
+    assert all(torch.isclose(d_.x, x_).all() for d_, x_ in zip(data, x))
+    assert all(len(d.edge_index) == 2 for d in data)
+    assert all(d.edge_attr is not None for d in data)
+    assert all([d.edge_index.shape[1] == d.edge_attr.shape[0]] for d in data)
+
+
 def test_additional_parameters_1():
     x = torch.rand(3, 10, 2)
     pos = torch.rand(3, 10, 2)
-    additional_parameters = {'y': torch.ones(3)}
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3,
-                        additional_params=additional_parameters)
+    additional_parameters = {"y": torch.ones(3)}
+    graph = RadiusGraph(
+        x=x,
+        pos=pos,
+        build_edge_attr=True,
+        r=0.3,
+        additional_params=additional_parameters,
+    )
     assert len(graph.data) == 3
     data = graph.data
     assert all(torch.isclose(d_.x, x_).all() for (d_, x_) in zip(data, x))
-    assert all(hasattr(d, 'y') for d in data)
+    assert all(hasattr(d, "y") for d in data)
     assert all(d_.y == 1 for d_ in data)
 
 
 @pytest.mark.parametrize(
     "additional_parameters",
     [
-        ({'y': torch.rand(3, 10, 1)}),
-        ({'y': [torch.rand(10, 1) for _ in range(3)]}),
-    ]
+        ({"y": torch.rand(3, 10, 1)}),
+        ({"y": [torch.rand(10, 1) for _ in range(3)]}),
+    ],
 )
 def test_additional_parameters_2(additional_parameters):
     x = torch.rand(3, 10, 2)
     pos = torch.rand(3, 10, 2)
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3,
-                        additional_params=additional_parameters)
+    graph = RadiusGraph(
+        x=x,
+        pos=pos,
+        build_edge_attr=True,
+        r=0.3,
+        additional_params=additional_parameters,
+    )
     assert len(graph.data) == 3
     data = graph.data
     assert all(torch.isclose(d_.x, x_).all() for (d_, x_) in zip(data, x))
-    assert all(hasattr(d, 'y') for d in data)
+    assert all(hasattr(d, "y") for d in data)
     assert all(torch.isclose(d_.x, x_).all() for (d_, x_) in zip(data, x))
+
+
+@pytest.mark.parametrize(
+    "additional_parameters",
+    [
+        ({"y": torch.rand(4, 10, 1)}),
+        ({"y": [torch.rand(10, 1) for _ in range(4)]}),
+    ],
+)
+def test_wrong_additional_parameters(additional_parameters):
+    x = torch.rand(3, 10, 2)
+    pos = torch.rand(3, 10, 2)
+    with pytest.raises(RuntimeError):
+        RadiusGraph(
+            x=x,
+            pos=pos,
+            build_edge_attr=True,
+            r=0.3,
+            additional_params=additional_parameters,
+        )
+
 
 def test_custom_build_edge_attr_func():
     x = torch.rand(3, 10, 2)
@@ -152,12 +215,20 @@ def test_custom_build_edge_attr_func():
     def build_edge_attr(x, pos, edge_index):
         return torch.cat([pos[edge_index[0]], pos[edge_index[1]]], dim=-1)
 
-    graph = RadiusGraph(x=x, pos=pos, build_edge_attr=True, r=.3,
-                        custom_build_edge_attr=build_edge_attr)
+    graph = RadiusGraph(
+        x=x,
+        pos=pos,
+        build_edge_attr=True,
+        r=0.3,
+        custom_build_edge_attr=build_edge_attr,
+    )
     assert len(graph.data) == 3
     data = graph.data
-    assert all(hasattr(d, 'edge_attr') for d in data)
+    assert all(hasattr(d, "edge_attr") for d in data)
     assert all(d.edge_attr.shape[1] == 4 for d in data)
-    assert all(torch.isclose(d.edge_attr,
-                             build_edge_attr(d.x, d.pos, d.edge_index)).all()
-               for d in data)
+    assert all(
+        torch.isclose(
+            d.edge_attr, build_edge_attr(d.x, d.pos, d.edge_index)
+        ).all()
+        for d in data
+    )
