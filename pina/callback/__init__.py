9b08a1c6fa5b0f0d87c54428daedc9f59f16f292
Giovanni Canali <115086358+GiovanniCanali@users.noreply.github.com>
2025-03-06 14:41:34 +0100

    add linear weight update callback (#474)

--- pina/callback/__init__.py
+++ pina/callback/__init__.py
@@ -3,8 +3,10 @@ __all__ = [
     "R3Refinement",
     "MetricTracker",
     "PINAProgressBar",
+    "LinearWeightUpdate",
 ]
 
 from .optimizer_callback import SwitchOptimizer
 from .adaptive_refinement_callback import R3Refinement
 from .processing_callback import MetricTracker, PINAProgressBar
+from .linear_weight_update_callback import LinearWeightUpdate
