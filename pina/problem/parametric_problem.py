02067fadb78a20a9a88644083b684c596a28fdef
giovanni <giovanni.canali98@yahoo.it>
2025-03-13 12:04:02 +0100

    fix problem doc

--- pina/problem/parametric_problem.py
+++ pina/problem/parametric_problem.py
@@ -7,26 +7,23 @@ from .abstract_problem import AbstractProblem
 
 class ParametricProblem(AbstractProblem):
     """
-    The class for the definition of parametric problems, i.e., problems
-    with parameters among the input variables.
-
-    Here's an example of a spatial parametric ODE problem, i.e., a spatial
-    ODE problem with an additional parameter `alpha` as coefficient of the
-    derivative term.
-
-    :Example:
-        TODO
+    Class for defining parametric problems, where certain input variables are
+    treated as parameters that can vary, allowing the model to adapt to
+    different scenarios based on the chosen parameters.
     """
 
     @abstractmethod
     def parameter_domain(self):
         """
-        The parameters' domain of the problem.
+        The domain of the parameters of the problem.
         """
 
     @property
     def parameters(self):
         """
-        The parameters' variables of the problem.
+        Get the parameters of the problem.
+
+        :return: The parameters of the problem.
+        :rtype: list[str]
         """
         return self.parameter_domain.variables
