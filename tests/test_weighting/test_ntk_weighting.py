b3f7158afb77fcc287354303ca5b6a1c19617e3a
AleDinve <giuseppealessio.d@student.unisi.it>
2025-03-19 08:27:17 -0400

    test fix

--- tests/test_weighting/test_ntk_weighting.py
+++ tests/test_weighting/test_ntk_weighting.py
@@ -6,27 +6,33 @@ from pina.problem.zoo import Poisson2DSquareProblem
 from pina.loss import NeuralTangetKernelWeighting
 
 problem = Poisson2DSquareProblem()
-model = FeedForward(len(problem.input_variables), len(problem.output_variables))
 condition_names = problem.conditions.keys()
-# print(problem.conditions.keys())
 
-
+@pytest.mark.parametrize(
+    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),0.5)]
+)
 def test_constructor(model, alpha):
     NeuralTangetKernelWeighting(model=model, alpha=alpha)
 
-
-def test_wrong_constructor1(alpha):
+@pytest.mark.parametrize(
+    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),1.2)]
+)
+def test_wrong_constructor2(model,alpha):
     with pytest.raises(ValueError):
         NeuralTangetKernelWeighting(model, alpha)
 
-
-def test_wrong_constructor2(model):
+@pytest.mark.parametrize(
+    "model,alpha", [(0.5,0.5)]
+)
+def test_wrong_constructor1(model):
     with pytest.raises(TypeError):
         NeuralTangetKernelWeighting(model)
 
-
-def test_train_aggregation(model):
-    weighting = NeuralTangetKernelWeighting(model=model, alpha=0.5)
+@pytest.mark.parametrize(
+    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),0.5)]
+)
+def test_train_aggregation(model,alpha):
+    weighting = NeuralTangetKernelWeighting(model=model, alpha=alpha)
     problem.discretise_domain(50)
     solver = PINN(problem=problem, model=model, weighting=weighting)
     trainer = Trainer(solver=solver, max_epochs=5, accelerator="cpu")
