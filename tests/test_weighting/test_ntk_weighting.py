f317bf0bda4baf461fbdb6d0d4db93c18b350f1c
AleDinve <giuseppealessio.d@student.unisi.it>
2025-03-19 08:28:39 -0400

    black code formatter

--- tests/test_weighting/test_ntk_weighting.py
+++ tests/test_weighting/test_ntk_weighting.py
@@ -8,30 +8,56 @@ from pina.loss import NeuralTangetKernelWeighting
 problem = Poisson2DSquareProblem()
 condition_names = problem.conditions.keys()
 
+
 @pytest.mark.parametrize(
-    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),0.5)]
+    "model,alpha",
+    [
+        (
+            FeedForward(
+                len(problem.input_variables), len(problem.output_variables)
+            ),
+            0.5,
+        )
+    ],
 )
 def test_constructor(model, alpha):
     NeuralTangetKernelWeighting(model=model, alpha=alpha)
 
+
 @pytest.mark.parametrize(
-    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),1.2)]
+    "model,alpha",
+    [
+        (
+            FeedForward(
+                len(problem.input_variables), len(problem.output_variables)
+            ),
+            1.2,
+        )
+    ],
 )
-def test_wrong_constructor2(model,alpha):
+def test_wrong_constructor2(model, alpha):
     with pytest.raises(ValueError):
         NeuralTangetKernelWeighting(model, alpha)
 
-@pytest.mark.parametrize(
-    "model,alpha", [(0.5,0.5)]
-)
+
+@pytest.mark.parametrize("model,alpha", [(0.5, 0.5)])
 def test_wrong_constructor1(model):
     with pytest.raises(TypeError):
         NeuralTangetKernelWeighting(model)
 
+
 @pytest.mark.parametrize(
-    "model,alpha", [(FeedForward(len(problem.input_variables), len(problem.output_variables)),0.5)]
+    "model,alpha",
+    [
+        (
+            FeedForward(
+                len(problem.input_variables), len(problem.output_variables)
+            ),
+            0.5,
+        )
+    ],
 )
-def test_train_aggregation(model,alpha):
+def test_train_aggregation(model, alpha):
     weighting = NeuralTangetKernelWeighting(model=model, alpha=alpha)
     problem.discretise_domain(50)
     solver = PINN(problem=problem, model=model, weighting=weighting)
