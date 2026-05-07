14b218490012eb772bf5c5e91c35c9b46378fdd8
GiovanniCanali <giovanni.canali98@yahoo.it>
2026-05-07 12:05:17 +0200

    fix aggregator + add tests

--- pina/data/__init__.py
+++ pina/data/__init__.py
@@ -1,6 +1,7 @@
 """Module containing utilities for dataset and data loader management."""
 
-__all__ = ["PinaDataModule", "_SingleBatchDataLoader"]
+__all__ = ["PinaDataModule", "_SingleBatchDataLoader", "_Aggregator"]
 
 from pina._src.data.data_module import PinaDataModule
 from pina._src.data.single_batch_data_loader import _SingleBatchDataLoader
+from pina._src.data.aggregator import _Aggregator
