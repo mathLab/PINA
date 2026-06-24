acd4d2888a41dd7f49b86d368b9d16685242800c
ndem0 <ndem0@users.noreply.github.com>
2026-06-24 13:38:20 +0000

    export tutorials changed in ccff5d1

--- tutorials/tutorial13/tutorial.py
+++ tutorials/tutorial13/tutorial.py
@@ -2,15 +2,15 @@
 # coding: utf-8
 
 # # Tutorial: Learning Multiscale PDEs Using Fourier Feature Networks
-#
+# 
 # [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/mathLab/PINA/blob/master/tutorials/tutorial13/tutorial.ipynb)
-#
+# 
 # This tutorial demonstrates how to solve a PDE with multiscale behavior using Physics-Informed Neural Networks (PINNs), as discussed in [*On the Eigenvector Bias of Fourier Feature Networks: From Regression to Solving Multi-Scale PDEs with Physics-Informed Neural Networks*](https://doi.org/10.1016/j.cma.2021.113938).
-#
+# 
 # Let’s begin by importing the necessary libraries.
-#
+# 
 
-# In[1]:
+# In[ ]:
 
 
 ## routine needed to run the notebook on Google Colab
@@ -29,10 +29,13 @@ import warnings
 
 from pina import Condition, Trainer
 from pina.problem import SpatialProblem
-from pina.solver import PINN, SelfAdaptivePINN as SAPINN
+from pina.solver import (
+    PhysicsInformedSingleModelSolver,
+    SelfAdaptivePhysicsInformedSolver,
+)
 from pina.loss import LpLoss
 from pina.domain import CartesianDomain
-from pina.equation import FixedValue, Poisson
+from pina.equation.zoo import FixedValue, PoissonEquation
 from pina.model import FeedForward
 from pina.model.block import FourierFeatureEmbedding
 
@@ -40,34 +43,34 @@ warnings.filterwarnings("ignore")
 
 
 # ## Multiscale Problem
-#
+# 
 # We begin by presenting the problem, which is also discussed in Section 2 of [*On the Eigenvector Bias of Fourier Feature Networks: From Regression to Solving Multi-Scale PDEs with Physics-Informed Neural Networks*](https://doi.org/10.1016/j.cma.2021.113938). The one-dimensional Poisson problem we aim to solve is mathematically defined as:
-#
+# 
 # \begin{equation}
 # \begin{cases}
 # \Delta u(x) + f(x) = 0 \quad x \in [0,1], \\
 # u(x) = 0 \quad x \in \partial[0,1],
 # \end{cases}
 # \end{equation}
-#
+# 
 # We define the solution as:
-#
+# 
 # $$
 # u(x) = \sin(2\pi x) + 0.1 \sin(50\pi x),
 # $$
-#
+# 
 # which leads to the corresponding force term:
-#
+# 
 # $$
 # f(x) = (2\pi)^2 \sin(2\pi x) + 0.1 (50 \pi)^2 \sin(50\pi x).
 # $$
-#
+# 
 # While this example is simple and pedagogical, it's important to note that the solution exhibits low-frequency behavior in the macro-scale and high-frequency behavior in the micro-scale. This characteristic is common in many practical scenarios.
-#
+# 
 # Below is the implementation of the `Poisson` problem as described mathematically above.
 # > **👉 We have a dedicated [tutorial](https://mathlab.github.io/PINA/tutorial16/tutorial.html) to teach how to build a Problem from scratch — have a look if you're interested!**
 
-# In[2]:
+# In[ ]:
 
 
 def forcing_term(x):
@@ -77,7 +80,7 @@ def forcing_term(x):
     )
 
 
-poisson_equation = Poisson(forcing_term=forcing_term)
+poisson_equation = PoissonEquation(forcing_term=forcing_term)
 
 
 class Poisson(SpatialProblem):
@@ -106,18 +109,18 @@ problem.discretise_domain(128, "grid", domains="phys_cond")
 problem.discretise_domain(2, "grid", domains="boundary")
 
 
-# A standard PINN approach would involve fitting the model using a Feed Forward (fully connected) Neural Network. For a conventional fully-connected neural network, it is relatively easy to approximate a function $u$, given sufficient data inside the computational domain.
-#
+# A standard Physics-Informed approach would involve fitting the model using a Feed Forward (fully connected) Neural Network. For a conventional fully-connected neural network, it is relatively easy to approximate a function $u$, given sufficient data inside the computational domain. 
+# 
 # However, solving high-frequency or multi-scale problems presents significant challenges to PINNs, especially when the number of data points is insufficient to capture the different scales effectively.
-#
-# Below, we run a simulation using both the `PINN` solver and the self-adaptive `SAPINN` solver, employing a [`FeedForward`](https://mathlab.github.io/PINA/_modules/pina/model/feed_forward.html#FeedForward) model.
-#
+# 
+# Below, we run a simulation using both the `PhysicsInformedSingleModelSolver` solver and the `SelfAdaptivePhysicsInformedSolver` solver, employing a [`FeedForward`](https://mathlab.github.io/PINA/_modules/pina/model/feed_forward.html#FeedForward) model.
+# 
 
 # In[ ]:
 
 
-# training with PINN and visualize results
-pinn = PINN(
+# training with PhysicsInformedSingleModelSolver and visualize results
+pinn = PhysicsInformedSingleModelSolver(
     problem=problem,
     model=FeedForward(
         input_dimensions=1, output_dimensions=1, layers=[100, 100, 100]
@@ -135,8 +138,8 @@ trainer = Trainer(
 )
 trainer.train()
 
-# training with PINN and visualize results
-sapinn = SAPINN(
+# training with SelfAdaptivePhysicsInformedSolver and visualize results
+sapinn = SelfAdaptivePhysicsInformedSolver(
     problem=problem,
     model=FeedForward(
         input_dimensions=1, output_dimensions=1, layers=[100, 100, 100]
@@ -154,7 +157,7 @@ trainer_sapinn = Trainer(
 trainer_sapinn.train()
 
 
-# In[4]:
+# In[ ]:
 
 
 # define the function to plot the solution obtained using matplotlib
@@ -168,6 +171,7 @@ def plot_solution(pinn_to_use, title):
     plt.plot(pts.extract(["x"]), true_output, label="True solution")
     plt.title(title)
     plt.legend()
+    plt.show()
 
 
 # plot the solution of the two PINNs
@@ -176,13 +180,11 @@ plt.figure()
 plot_solution(sapinn, "Self Adaptive PINN solution")
 
 
-# We can clearly observe that neither of the two solvers has successfully learned the solution.
-# The issue is not with the optimization strategy (i.e., the solver), but rather with the model used to solve the problem.
-# A simple `FeedForward` network struggles to handle multiscale problems, especially when there are not enough collocation points to capture the different scales effectively.
-#
-# Next, let's compute the $l_2$ relative error for both the `PINN` and `SAPINN` solutions:
+# We can clearly observe that neither of the two solvers has successfully learned the solution. The issue is not with the optimization strategy (i.e., the solver), but rather with the model used to solve the problem. A simple `FeedForward` network struggles to handle multiscale problems, especially when there are not enough collocation points to capture the different scales effectively.
+# 
+# Next, let's compute the $l_2$ relative error for both the `PhysicsInformedSingleModelSolver` and `SelfAdaptivePhysicsInformedSolver` solutions:
 
-# In[5]:
+# In[ ]:
 
 
 # l2 loss from PINA losses
@@ -199,22 +201,22 @@ print(
 
 
 # Which is indeed very high!
-#
+# 
 # ## Fourier Feature Embedding in PINA
 # Fourier Feature Embedding is a technique used to transform the input features, aiding the network in learning multiscale variations in the output. It was first introduced in [*On the Eigenvector Bias of Fourier Feature Networks: From Regression to Solving Multi-Scale PDEs with Physics-Informed Neural Networks*](https://doi.org/10.1016/j.cma.2021.113938), where it demonstrated excellent results for multiscale problems.
-#
+# 
 # The core idea behind Fourier Feature Embedding is to map the input $\mathbf{x}$ into an embedding $\tilde{\mathbf{x}}$, defined as:
-#
+# 
 # $$
 # \tilde{\mathbf{x}} = \left[\cos\left( \mathbf{B} \mathbf{x} \right), \sin\left( \mathbf{B} \mathbf{x} \right)\right],
 # $$
-#
+# 
 # where $\mathbf{B}_{ij} \sim \mathcal{N}(0, \sigma^2)$. This simple operation allows the network to learn across multiple scales!
-#
+# 
 # In **PINA**, we have already implemented this feature as a `layer` called [`FourierFeatureEmbedding`](https://mathlab.github.io/PINA/_rst/layers/fourier_embedding.html). Below, we will build the *Multi-scale Fourier Feature Architecture*. In this architecture, multiple Fourier feature embeddings (initialized with different $\sigma$ values) are applied to the input coordinates. These embeddings are then passed through the same fully-connected neural network, and the outputs are concatenated with a final linear layer.
-#
+# 
 
-# In[6]:
+# In[ ]:
 
 
 class MultiscaleFourierNet(torch.nn.Module):
@@ -237,13 +239,15 @@ class MultiscaleFourierNet(torch.nn.Module):
         return self.final_layer(torch.cat([e1, e2], dim=-1))
 
 
-# We will train the `MultiscaleFourierNet` using the `PINN` solver.
-# Feel free to experiment with other PINN variants as well, such as `SAPINN`, `GPINN`, `CompetitivePINN`, and others, to see how they perform on this multiscale problem.
+# We will train the `MultiscaleFourierNet` using the `PhysicsInformedSingleModelSolver` solver.  
+# Feel free to experiment with other PINN variants as well, such as `SelfAdaptivePhysicsInformedSolver`, `GradientPhysicsInformedSingleModelSolver`, `CompetitivePhysicsInformedSolver`, and others, to see how they perform on this multiscale problem.
 
 # In[ ]:
 
 
-multiscale_pinn = PINN(problem=problem, model=MultiscaleFourierNet())
+multiscale_pinn = PhysicsInformedSingleModelSolver(
+    problem=problem, model=MultiscaleFourierNet()
+)
 trainer = Trainer(
     multiscale_pinn,
     max_epochs=1500,
@@ -258,7 +262,7 @@ trainer.train()
 
 # Let us now plot the solution and compute the relative $l_2$ again!
 
-# In[8]:
+# In[ ]:
 
 
 # plot solution obtained
@@ -272,17 +276,17 @@ print(
 
 
 # It is clear that the network has learned the correct solution, with a very low error. Of course, longer training and a more expressive neural network could further improve the results!
-#
+# 
 # ## What's Next?
-#
+# 
 # Congratulations on completing the one-dimensional Poisson tutorial of **PINA** using `FourierFeatureEmbedding`! There are many potential next steps you can explore:
-#
+# 
 # 1. **Train the network longer or with different layer sizes**: Experiment with different configurations to improve accuracy.
-#
+# 
 # 2. **Understand the role of `sigma` in `FourierFeatureEmbedding`**: The original paper provides insightful details on the impact of `sigma`. It's a good next step to dive deeper into its effect.
-#
+# 
 # 3. **Implement the *Spatio-temporal Multi-scale Fourier Feature Architecture***: Code this architecture for a more complex, time-dependent PDE (refer to Section 3 of the original paper).
-#
+# 
 # 4. **...and many more!**: There are countless directions to further explore, from testing on different problems to refining the model architecture.
-#
+# 
 # For more resources and tutorials, check out the [PINA Documentation](https://mathlab.github.io/PINA/).
--- tutorials/tutorial16/tutorial.py
+++ tutorials/tutorial16/tutorial.py
@@ -2,19 +2,19 @@
 # coding: utf-8
 
 # # Tutorial: How to build a Problem in PINA
-#
+# 
 # [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/mathLab/PINA/blob/master/tutorials/tutorial16/tutorial.ipynb)
-#
+# 
 
 # In this tutorial, we will demonstrate how to build a **Problem** in **PINA** using a toy example. The tutorial will cover the following topics:
-#
+# 
 # - **Building a Problem**: Learn how to construct a problem using the built-in PINA classes.
 # - **Generating Data for Physics-Informed Training**: Understand how to generate the necessary data for training.
 # - **Exploring the `problem.zoo` Module**: Get familiar with the `problem.zoo` module, which collects pre-built problems for easy use.
-#
+# 
 # By the end of this tutorial, you'll be able to write **data-driven** or **differential problems** in **PINA** and prepare them for model training!
 
-# In[1]:
+# In[ ]:
 
 
 ## routine needed to run the notebook on Google Colab
@@ -37,38 +37,38 @@ warnings.filterwarnings("ignore")
 # ## Build a PINA problem
 
 # In **PINA**, defining a problem is done by creating a Python `class` that inherits from one or more problem classes, such as `SpatialProblem`, `TimeDependentProblem`, or `ParametricProblem`, depending on the nature of the problem. We refer to the `model` as the object that solves the problem, e.g., a **Neural Network**.
-#
+# 
 # We can have two types of problems:
 # 1. ***Data-Driven Problems***: The model is trained using data, such as in classification networks or autoencoders.
 # 2. ***Physics-Driven Problems***: The model is trained using physical laws representing the problem, such as in **PINNs**.
-# Let's start by building the first type, the data driven type.
-#
+# Let's start by building the first type, the data driven type. 
+# 
 # ### Data driven modelling
 # In data-driven modelling, we always have an **input** and a **target**. The model's objective is to reconstruct the target from the input. Examples include:
 # - Image reconstruction (perturbed image as input, clear image as target)
 # - Classification (e.g., input: molecule, target: chemical properties)
-#
-# To build a data-driven problem in **PINA**, you can inherit from the `AbstractProblem` class. Below is an example of a regression problem where the input is a scalar value `x` and the target is a scalar value `y`.
-#
+# 
+# To build a data-driven problem in **PINA**, you can inherit from the `BaseProblem` class. Below is an example of a regression problem where the input is a scalar value `x` and the target is a scalar value `y`.
+# 
 # ```python
-# from pina.problem import AbstractProblem
-#
-# class SupervisedProblem(AbstractProblem):
-#
+# from pina.problem import BaseProblem
+# 
+# class SupervisedProblem(BaseProblem):
+#     
 #     input_variables = ['x']
 #     output_variables = ['y']
-#
+# 
 #     # other stuff ...
 # ```
 # Observe that we define `input_variables` and `output_variables` as lists of symbols. This is because, in PINA, `torch.Tensors` can be labeled (see [`LabelTensor`](https://mathlab.github.io/PINA/_rst/label_tensor.html)), providing maximum flexibility for tensor manipulation. If you prefer to use regular tensors, you can simply set these to ``None``.
-#
+# 
 # To specify the input and target data, you need to use the [`Condition`](https://mathlab.github.io/PINA/_rst/condition/condition.html) interface. A condition defines the constraints (such as physical equations, boundary conditions, etc.) that must be satisfied within the problem. Once the condition is applied, the full problem is outlined below:
 
-# In[2]:
+# In[ ]:
 
 
 from pina import Condition, LabelTensor
-from pina.problem import AbstractProblem
+from pina.problem import BaseProblem
 
 # creating some fictitious data
 input_1 = LabelTensor(torch.randn(10, 1), "x")  # <== input_variables
@@ -77,7 +77,7 @@ target_1 = LabelTensor(torch.randn(10, 1), "y")  # <== output_variables
 target_2 = LabelTensor(torch.randn(10, 1), "y")  # <== output_variables
 
 
-class SupervisedProblem(AbstractProblem):
+class SupervisedProblem(BaseProblem):
 
     input_variables = ["x"]
     output_variables = ["y"]
@@ -91,14 +91,11 @@ class SupervisedProblem(AbstractProblem):
 problem = SupervisedProblem()
 
 
-# You can define as many conditions as needed, and the model will attempt to minimize all of them simultaneously! You can access the data in various ways:
-#
-# - `problem.conditions['<condition name>'].input`, `problem.conditions['<condition name>'].target` – Access the input and output data for the specified condition `<condition name>`.
-# - `problem.input_pts` – Access the input points for all conditions.
-#
+# You can define as many conditions as needed, and the model will attempt to minimize all of them simultaneously! You can access the input and target data for a specified condition as follows: `problem.conditions['<condition name>'].input`, `problem.conditions['<condition name>'].target`.
+# 
 # To ensure that the problem is ready, you can check if all domains have been discretized, meaning all conditions have input points available to pass to the model:
 
-# In[3]:
+# In[ ]:
 
 
 # check if all domains are discretised
@@ -109,7 +106,7 @@ problem.are_all_domains_discretised
 
 # ### Simple Ordinary Differential Equation
 # What if we don't have data but we know the physical laws that define the data? Then physics-informed training is the solution! As an example, consider the following Ordinary Differential Equation (ODE):
-#
+# 
 # $$
 # \begin{equation}
 # \begin{cases}
@@ -118,39 +115,39 @@ problem.are_all_domains_discretised
 # \end{cases}
 # \end{equation}
 # $$
-#
+# 
 # with the analytical solution $u(x) = e^x$. This problem is a spatial problem because the ODE depends only on the spatial variable $x\in(0,1)$. In PINA, differential problems are categorized by their nature, e.g.:
 # * `SpatialProblem` $\rightarrow$ a differential equation with spatial variable(s)
 # * `TimeDependentProblem` $\rightarrow$ a time-dependent differential equation with temporal variable(s)
 # * `ParametricProblem` $\rightarrow$ a parametrized differential equation with parametric variable(s)
 # * `InverseProblem` $\rightarrow$ this is a more advanced topic, see [this tutorial](https://mathlab.github.io/PINA/tutorial7/tutorial.html) for more details.
-#
+# 
 # In our case, the physical ODE inherits from the `SpatialProblem` class, since only spatial variables define the ODE.
-#
+# 
 # ```python
 # class SimpleODE(SpatialProblem):
-#
+#     
 #     output_variables = ['u']
 #     spatial_domain = CartesianDomain{'x': [0, 1]})
-#
+# 
 #     # other stuff ...
 # ```
-#
+# 
 # What if our equation is was also time-dependent, e.g. Partial Differential Equations (PDE)? In this case, our `class` will inherit from both `SpatialProblem` and `TimeDependentProblem`:
-#
-#
+# 
+# 
 # ```python
 # class TimeSpaceODE(SpatialProblem, TimeDependentProblem):
-#
+# 
 #     output_variables = ["u"]
 #     spatial_domain = CartesianDomain({"x": [0, 1]})
 #     temporal_domain = CartesianDomain({"t": [0, 1]})
-#
+# 
 #     # other stuff ...
 # ```
-#
+# 
 # Differently from data-driven problems, differential-problems need to specify the domain type. If you look at our ODE definition, the spatial varibale $x$ is defined in the interval $(0,1)$, and accordingly the `spatial_domain` is a `CartesianDomain` with the input variable `x` in `[0,1]`. To know more about the Domain class see the [related tutorial](https://mathlab.github.io/PINA/tutorial6/tutorial.html). Different problems require different domain, here below we summarize the relevant ones:
-#
+# 
 # | Problem Type            | Required Domain                |
 # |-------------------------|--------------------------------|
 # | `SpatialProblem`        | `spatial_domain`              |
@@ -160,13 +157,14 @@ problem.are_all_domains_discretised
 
 # Nice, the Problem class is initialized! How to represent the differential equation in **PINA**? To do this, we need to load the **PINA** operators from `pina.operator` module. Again, we'll consider Equation (1) and represent it in **PINA**:
 
-# In[4]:
+# In[ ]:
 
 
 from pina.problem import SpatialProblem
 from pina.operator import grad
 from pina.domain import CartesianDomain
-from pina.equation import Equation, FixedValue
+from pina.equation import Equation
+from pina.equation.zoo import FixedValue
 
 
 # defining the ode equation
@@ -203,61 +201,49 @@ class SimpleODE(SpatialProblem):
         return torch.exp(pts.extract(["x"]))
 
 
-# As you can see, we implemented the `ode_equation` function which given the model ouput and input returns the equation residual. These residuals are the ones minimized during PINN optimization (for more on PINN see [the related tutorials](https://mathlab.github.io/PINA/_tutorial.html#physics-informed-neural-networks)).
-#
+# As you can see, we implemented the `ode_equation` function which given the model ouput and input returns the equation residual. These residuals are the ones minimized during PINN optimization (for more on PINN see [the related tutorials](https://mathlab.github.io/PINA/_tutorial.html#physics-informed-neural-networks)). 
+# 
 # How are the residuals computed?
-# Given the output we perform differential operation using the [operator modulus](https://mathlab.github.io/PINA/_rst/operator.html). It is pretty intuitive, each differential operator takes the following inputs:
-# - A tensor on which the operator is applied.
-# - A tensor with respect to which the operator is computed.
-# - The names of the output variables for which the operator is evaluated.
+# Given the output we perform differential operation using the [operator modulus](https://mathlab.github.io/PINA/_rst/operator.html). It is pretty intuitive, each differential operator takes the following inputs: 
+# - A tensor on which the operator is applied. 
+# - A tensor with respect to which the operator is computed. 
+# - The names of the output variables for which the operator is evaluated. 
 # - The names of the variables with respect to which the operator is computed.
-# We also have a `fast` version of differential operators, where no checks are performed. This can be used to boost performances, once you know the standard ones are doing their job.
-#
+# We also have a `fast` version of differential operators, where no checks are performed. This can be used to boost performances, once you know the standard ones are doing their job. 
+# 
 # Notice that we do not pass directly a `python` function, but an `Equation` object, which is initialized with the `python` function. This is done so that all the computations and internal checks are done inside **PINA**, see [the related tutorials](https://mathlab.github.io/PINA/tutorial12/tutorial.html) for more.
-#
+# 
 # Once we have defined the function, we need to tell the neural network where these methods are to be applied. To do so, we use again the `Condition` class. In the `Condition` class, we pass the location points and the equation we want minimized on those points.
-#
-# Finally, it's possible to define a `solution` function, which can be useful if we want to plot the results and see how the real solution compares to the expected (true) solution. Notice that the `solution` function is a method of the `Problem` class, but it is not mandatory for problem definition.
-#
+# 
+# Finally, it's possible to define a `solution` function, which can be useful if we want to plot the results and see how the real solution compares to the expected (true) solution. Notice that the `solution` function is a method of the problem class, but it is not mandatory for problem definition.
+# 
 
 # ## Generate data for Physical Problems
-#
-# When training physics based models, data can come in form of direct numerical simulation results (tensors, graph), or points in the domains which need to be sampled. In case we perform unsupervised learning, we just need the collocation points for training, i.e. points where we want to evaluate the neural network. Sampling point in **PINA** is very easy. But first, let's check if the domains are dicsretized by using the `are_all_domains_discretised` method.
+# 
+# When training physics-based models, data can come in form of direct numerical simulation results (tensors, graph), or points in the domains which need to be sampled. In case we perform unsupervised learning, we just need the collocation points for training, i.e. points where we want to evaluate the neural network. Sampling point in **PINA** is very easy. But first, let's check if the domains are discretized by using the `are_all_domains_discretised` method.
 
-# In[5]:
+# In[ ]:
 
 
 problem = SimpleODE()
 problem.are_all_domains_discretised
 
 
-# This is false becase the input points are not available (we need to discretize!). If you call `problem.input_points` at this stage you will get an error due to point missing in the condition.
-#
-# ```bash
-# >>> problem.input_pts
-# ```
-# ```python
-# ---------------------------------------------------------------------------
-# KeyError                                  Traceback (most recent call last)
-# Cell In[32], line 1
-# ----> 1 problem.input_pts
-#
-# File ~/GitHub/PINA/pina/problem/abstract_problem.py:78, in AbstractProblem.input_pts(self)
-#      76         to_return[cond_name] = cond.input
-#      77     elif hasattr(cond, "domain"):
-# ---> 78         to_return[cond_name] = self._discretised_domains[cond.domain]
-#      79 return to_return
-#
-# KeyError: 'x0'
-# ```
+# This is `False` since no domain has been discretized. One can check which domains have been discretized calling `problem.discretised_domains`:
 
-# To discretise the problem you can use the `discretise_domain` method:
+# In[ ]:
 
-# In[6]:
+
+problem.discretised_domains
+
+
+# To discretise the domains in the problem you can use the `discretise_domain` method:
+
+# In[ ]:
 
 
 # sampling 20 points in [0, 1] through discretization in all locations
-problem.discretise_domain(n=20, mode="grid", domains="all")
+problem.discretise_domain(n=20, mode="grid")
 
 # sampling 20 points in (0, 1) through latin hypercube sampling in D, and 1 point in x0
 problem.discretise_domain(n=20, mode="latin", domains=["D"])
@@ -269,7 +255,7 @@ problem.discretise_domain(n=20, mode="random")
 
 # We are going to use latin hypercube points for sampling. We need to sample in all the conditions domains. In our case we sample in `D` and `x0`.
 
-# In[7]:
+# In[ ]:
 
 
 # sampling for training
@@ -277,35 +263,37 @@ problem.discretise_domain(1, "random", domains=["x0"])
 problem.discretise_domain(5, "lh", domains=["D"])
 
 
-# The points are saved in a python `dict`, and can be accessed by calling the attributes `input_pts` or `discretised_domains` of the problem.
+# The points are saved in a python `dict`, and can be accessed by calling the `discretised_domains` of the problem.
 
-# In[8]:
+# In[ ]:
 
 
-print("Input points:", problem.input_pts)
 print("Input points labels:", problem.discretised_domains)
 
 
 # To visualize the sampled points we can use `matplotlib.pyplot`:
 
-# In[9]:
+# In[ ]:
 
 
-for location in problem.input_pts:
+for location in problem.discretised_domains:
     coords = (
-        problem.input_pts[location].extract(problem.spatial_variables).flatten()
+        problem.discretised_domains[location]
+        .extract(problem.spatial_variables)
+        .flatten()
     )
     plt.scatter(coords, torch.zeros_like(coords), s=10, label=location)
 plt.legend()
+plt.show()
 
 
 # ## The Problem Zoo module
-#
+# 
 # In PINA many problems are already implemented for you in the [Problem Zoo module](https://mathlab.github.io/PINA/_rst/_code.html#problems-zoo). For example, the supervised problem at the beginning of the tutorial is implemented in [`SupervisedProblem`](https://mathlab.github.io/PINA/_rst/problem/zoo/supervised_problem.html)!
-#
+# 
 # Let's see now a physics based example, the advection equation
 
-# In[10]:
+# In[ ]:
 
 
 from pina.problem.zoo import AdvectionProblem
@@ -324,13 +312,13 @@ print(
 
 
 # ## What's Next?
-#
+# 
 # Congratulations on completing the introductory tutorial of **PINA** problems! There are several directions you can explore next:
-#
+# 
 # 1. **Create Custom Problems**: Try building your own problems using the PINA framework, experiment with different PDEs, initial/boundary conditions, and data structures.
-#
+# 
 # 2. **Explore the Problem Zoo**: Dive into the [`problem.zoo` module](https://mathlab.github.io/PINA/_rst/_code.html#problems-zoo) to find a variety of predefined problem setups and use them as a starting point or inspiration for your own.
-#
+# 
 # 3. **...and many more!**: The possibilities are vast! Consider experimenting with different solver strategies, model architectures, or even implementing your own physical constraints.
-#
+# 
 # For more examples and in-depth guides, be sure to check out the [PINA Documentation](https://mathlab.github.io/PINA/).
